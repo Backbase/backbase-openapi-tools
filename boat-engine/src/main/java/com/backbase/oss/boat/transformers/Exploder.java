a60bed4e2554d53d155f58a7e8e183403690b447
Bart Veenstra <bartv@backbase.com>
2020-04-03 14:30:28 +0200

    Added Integration Tests for Export-bom mojo

--- boat-engine/src/main/java/com/backbase/oss/boat/transformers/Exploder.java
+++ boat-engine/src/main/java/com/backbase/oss/boat/transformers/Exploder.java
@@ -11,20 +11,26 @@ import java.nio.file.Files;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 @Slf4j
+@UtilityClass
 public class Exploder {
 
     public static void explode(OpenAPI openAPI, File target) {
-        if (target.exists())
-            target.delete();
+        if (target.exists()) {
+            try {
+                Files.delete(target.toPath());
+            } catch (IOException e) {
+                throw new ExplodeException(e);
+            }
+        }
 
         target.mkdirs();
 
         Set<String> exploded = new HashSet<>();
-
 
         log.info("Exploding Open API: {}", openAPI.getInfo().getTitle());
 
@@ -33,19 +39,13 @@ public class Exploder {
         componentsDir.mkdirs();
         examplesDir.mkdirs();
 
-
-        openAPI.getComponents().getSchemas().forEach((name, schema) -> {
-            File output = new File(componentsDir, name + ".yaml");
-
-
-            // Go through all properties and write references to output
-
-            explode(openAPI, exploded, componentsDir, name, schema);
-        });
+        openAPI.getComponents().getSchemas()
+            .forEach((name, schema) -> explode(openAPI, exploded, componentsDir, name, schema));
 
 
     }
 
+    @SuppressWarnings("java:S3776")
     private static void explode(OpenAPI openAPI, Set<String> exploded, File componentsDir, String name, Schema schema) {
         Map<String, Schema> properties = schema.getProperties();
         if (properties != null) {
@@ -56,14 +56,23 @@ public class Exploder {
                     Schema componentSchema = openAPI.getComponents().getSchemas().get(refName);
                     if (componentSchema != null && !exploded.contains(refName)) {
                         propertySchema.set$ref("../openapi.yaml#/components/schemas/" + refName);
-
                         write(componentsDir, refName, propertySchema);
                         exploded.add(refName);
                     }
                 }
             });
         }
-        if(schema instanceof ArraySchema) {
+        if (schema instanceof ArraySchema) {
+            String ref = ((ArraySchema) schema).getItems().get$ref();
+            if (ref != null) {
+                String refName = StringUtils.substringAfterLast(ref, "/");
+                Schema componentSchema = openAPI.getComponents().getSchemas().get(refName);
+                if (componentSchema != null && !exploded.contains(refName)) {
+                    ((ArraySchema) schema).getItems().set$ref("../openapi.yaml#/components/schemas/" + refName);
+                    write(componentsDir, refName, componentSchema);
+                    exploded.add(refName);
+                }
+            }
 
         }
 
@@ -82,7 +91,7 @@ public class Exploder {
             Files.write(new File(output, name + ".yaml").toPath(), schemaAsYaml.getBytes());
         } catch (IOException e) {
             log.error("Failed to write: {} to: {}", output.getName(), output);
-            throw new RuntimeException(e);
+            throw new ExplodeException(e);
         }
     }
 }
