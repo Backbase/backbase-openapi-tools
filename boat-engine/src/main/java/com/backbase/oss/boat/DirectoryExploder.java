c44c54ee9ae878a1cd655b7960fad63cdac9fb2f
marink <marink@backbase.com>
2020-05-22 16:51:28 +0200

    feat: put extracted examples in examples/<extracted-name>.json

--- boat-engine/src/main/java/com/backbase/oss/boat/DirectoryExploder.java
+++ boat-engine/src/main/java/com/backbase/oss/boat/DirectoryExploder.java
@@ -1,11 +1,27 @@
 package com.backbase.oss.boat;
 
+import com.backbase.oss.boat.example.NamedExample;
 import com.backbase.oss.boat.transformers.OpenAPIExtractor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.google.common.base.CaseFormat;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.validation.constraints.NotNull;
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 public class DirectoryExploder {
+
+    private static final Logger log = LoggerFactory.getLogger(DirectoryExploder.class);
 
     @NotNull
     private final OpenAPIExtractor openAPIExtractor;
@@ -14,6 +30,31 @@ public class DirectoryExploder {
         this.openAPIExtractor = openAPIExtractor;
     }
 
-    public void serializeIntoDirectory(@NotNull File outputDir) {
+    public void serializeIntoDirectory(@NotNull String outputDir) throws IOException {
+        List<NamedExample> examples = openAPIExtractor.extractExamples();
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter()); // todo - extract as field
+        Path outputPath = Paths.get(outputDir);
+        if (Files.notExists(outputPath)) {
+            Files.createDirectories(outputPath);
+        } else {
+            FileUtils.cleanDirectory(outputPath.toFile());
+        }
+        examples.forEach(namedExample -> {
+            String title = CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_HYPHEN, namedExample.getName());
+            String serializedValue = "";
+            try {
+                serializedValue = writer.writeValueAsString(namedExample.getExample());
+                Path exampleFile = outputPath.resolve(title + ".json");
+                if (Files.notExists(exampleFile)) {
+                    Files.createFile(exampleFile);
+                }
+                Files.write(exampleFile, serializedValue.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+            } catch (JsonProcessingException e) {
+                log.error("Could not serialize to JSON", e);
+            } catch (IOException e) {
+                log.error("Could not write to file: {}.\nError: {}.\nSerialized value: {}", title, e, serializedValue);
+            }
+        });
     }
 }
