94fc3058b85a5b5b8311dab5eab1ecc3226b37ba
marink <marink@backbase.com>
2020-05-23 17:21:01 +0200

    test: content and media type example extractors

--- boat-engine/src/test/java/com/backbase/oss/boat/transformers/CommonExtractorsTest.java
+++ boat-engine/src/test/java/com/backbase/oss/boat/transformers/CommonExtractorsTest.java
@@ -3,11 +3,16 @@ package com.backbase.oss.boat.transformers;
 import com.backbase.oss.boat.example.NamedExample;
 import io.swagger.v3.oas.models.examples.Example;
 import io.swagger.v3.oas.models.headers.Header;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.Encoding;
+import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.parameters.Parameter;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -119,5 +124,72 @@ public class CommonExtractorsTest {
                 namedExample.getExample().getValue() == parameter.getExample()
                         && namedExample.getName().equals("param-" + parameter.getName())
         ));
+    }
+
+    @Test
+    public void test_parameterExamples_ownExamplePresentAndExamplesMapNotEmptyAndContentExamples() {
+        Example example = new Example().value("some-value");
+        String mediaTypeExampleName = "MediaTypeExampleName1";
+        String mediaType$ref = "../openapi.yaml#/components/schemas/SomeName";
+        Example mediaTypeExample = new Example().value("some-data");
+        Parameter parameter = new Parameter()
+                .name("accountId")
+                .example("a1234-b5")
+                .addExample("example", example).content(new Content()
+                        .addMediaType("text/plain", new MediaType()
+                                .schema(new Schema<>().$ref(mediaType$ref))
+                                .addExamples(mediaTypeExampleName, mediaTypeExample)
+                        ));
+        List<NamedExample> examples = CommonExtractors.parameterExamples(parameter);
+        assertEquals(3, examples.size());
+        assertTrue(examples.stream().anyMatch(namedExample ->
+                namedExample.getExample() == example && namedExample.getName().equals(parameter.getName())
+        ));
+        assertTrue(examples.stream().anyMatch(namedExample ->
+                namedExample.getExample().getValue() == parameter.getExample()
+                        && namedExample.getName().equals("param-" + parameter.getName())
+        ));
+        assertTrue(examples.stream().anyMatch(namedExample ->
+                namedExample.getExample() == mediaTypeExample
+                        && namedExample.getName().equals("SomeName-" + mediaTypeExampleName)
+        ));
+    }
+
+    @Test
+    public void test_contentExamples_noMediaType() {
+        Content content = new Content();
+        List<NamedExample> examples = CommonExtractors.contentExamples(content);
+        assertTrue(examples.isEmpty());
+    }
+
+    @Test
+    public void test_contentExamples_mediaTypeAndItsEncodingPresent() {
+        String encodingExampleName = "jim";
+        Example encodingExample = new Example().value("blacksmith");
+        String headerSchemaName = "MySchema";
+        Map<String, Header> encodingHeadersExamples = new HashMap<>();
+        encodingHeadersExamples.put("X-Some-Header", new Header()
+                .addExample(encodingExampleName, encodingExample)
+                .schema(new Schema<>().name(headerSchemaName)));
+        Encoding encoding = new Encoding().headers(encodingHeadersExamples);
+        String mediaTypeExampleName = "john";
+        Example mediaTypeExample = new Example().value("carpenter");
+        String mediaTypeSchemaName = "MyMediaType";
+        MediaType mediaType = new MediaType()
+                .schema(new Schema<>().name(mediaTypeSchemaName))
+                .addExamples(mediaTypeExampleName, mediaTypeExample)
+                .addEncoding("my-encoding", encoding);
+        Content content = new Content()
+                .addMediaType("text/plain", mediaType);
+        List<NamedExample> examples = CommonExtractors.contentExamples(content);
+        assertEquals(2, examples.size());
+        assertTrue(examples.stream().anyMatch(
+                namedExample -> namedExample.getExample() == encodingExample
+                        && namedExample.getName().equals(headerSchemaName + "-" + encodingExampleName))
+        );
+        assertTrue(examples.stream().anyMatch(
+                namedExample -> namedExample.getExample() == mediaTypeExample
+                        && namedExample.getName().equals(mediaTypeSchemaName + "-" + mediaTypeExampleName))
+        );
     }
 }
