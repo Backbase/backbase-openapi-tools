858b8ad5f1045f395272d80a2113e15b207cfa39
Bart Veenstra <bartv@backbase.com>
2020-08-19 20:26:41 +0200

    Batch Diff now walks over specs from same directory!

--- boat-maven-plugin/src/main/java/com/backbase/oss/boat/DiffMojo.java
+++ boat-maven-plugin/src/main/java/com/backbase/oss/boat/DiffMojo.java
@@ -3,8 +3,13 @@ package com.backbase.oss.boat;
 import com.github.elibracha.OpenApiCompare;
 import com.github.elibracha.model.ChangedOpenApi;
 import com.github.elibracha.output.ConsoleRender;
+import com.github.elibracha.output.HtmlRender;
+import com.github.elibracha.output.MarkdownRender;
+import com.github.elibracha.output.Render;
 import java.io.File;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -29,6 +34,15 @@ public class DiffMojo extends AbstractMojo {
     @Parameter(name = "breakOnBreakingChanges", defaultValue = "true")
     private boolean breakOnBreakingChanges;
 
+    @Parameter(name = "writeChangelog", defaultValue = "false")
+    private boolean writeChangelog;
+
+    @Parameter(name = "changeLogRenderer", defaultValue = "markdown")
+    private String changelogRenderer;
+
+    @Parameter(name = "changelogOutput", defaultValue = "${project.build.directory}/changelog")
+    private File changelogOutput;
+
     @Override
     public void execute() throws MojoExecutionException {
         ChangedOpenApi changedOpenApi = OpenApiCompare.fromFiles(oldFile, newFile);
@@ -43,6 +57,26 @@ public class DiffMojo extends AbstractMojo {
             log.info("\n{}", consoleRender.render(changedOpenApi));
         }
 
+        if (writeChangelog) {
+            Render render;
+
+            String extension;
+            if ("markdown".equals(changelogRenderer)) {
+                render = new MarkdownRender();
+                extension = "md";
+            } else if ("html".equals(changelogRenderer)) {
+                render = new HtmlRender();
+                extension = "html";
+            } else {
+                throw new MojoExecutionException("Invalid changelogRender. Supported types are 'markdown' and 'html");
+            }
+            try {
+                FileUtils.write(new File(changelogOutput, "changelog." + extension), render.render(changedOpenApi));
+            } catch (IOException e) {
+                throw new MojoExecutionException("Failed to write markdown output", e);
+            }
+        }
+
     }
 
     public void setNewFile(File newFile) {
@@ -55,5 +89,29 @@ public class DiffMojo extends AbstractMojo {
 
     public void setBreakOnBreakingChanges(boolean breakOnBreakingChanges) {
         this.breakOnBreakingChanges = breakOnBreakingChanges;
+    }
+
+    public boolean isWriteChangelog() {
+        return writeChangelog;
+    }
+
+    public void setWriteChangelog(boolean writeChangelog) {
+        this.writeChangelog = writeChangelog;
+    }
+
+    public String getChangelogRenderer() {
+        return changelogRenderer;
+    }
+
+    public void setChangelogRenderer(String changelogRenderer) {
+        this.changelogRenderer = changelogRenderer;
+    }
+
+    public File getChangelogOutput() {
+        return changelogOutput;
+    }
+
+    public void setChangelogOutput(File changelogOutput) {
+        this.changelogOutput = changelogOutput;
     }
 }
