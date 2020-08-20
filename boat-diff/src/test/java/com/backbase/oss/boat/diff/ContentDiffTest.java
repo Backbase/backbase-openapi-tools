91ba689a0181a04bcc877d8cabe85034a87f886b
Bart Veenstra <bartv@backbase.com>
2020-08-20 13:28:00 +0200

    Added BOAT::DIFF

---
+++ boat-diff/src/test/java/com/backbase/oss/boat/diff/ContentDiffTest.java
@@ -0,0 +1,30 @@
+package com.backbase.oss.boat.diff;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.backbase.oss.boat.diff.model.ChangedOpenApi;
+import org.junit.jupiter.api.Test;
+
+public class ContentDiffTest {
+
+    private final String OPENAPI_DOC1 = "content_diff_1.yaml";
+    private final String OPENAPI_DOC2 = "content_diff_2.yaml";
+
+    @Test
+    public void testContentDiffWithOneEmptyMediaType() {
+        ChangedOpenApi changedOpenApi = OpenApiCompare.fromLocations(OPENAPI_DOC1, OPENAPI_DOC2);
+        assertThat(changedOpenApi.isIncompatible()).isTrue();
+    }
+
+    @Test
+    public void testContentDiffWithEmptyMediaTypes() {
+        ChangedOpenApi changedOpenApi = OpenApiCompare.fromLocations(OPENAPI_DOC1, OPENAPI_DOC1);
+        assertThat(changedOpenApi.isUnchanged()).isTrue();
+    }
+
+    @Test
+    public void testSameContentDiff() {
+        ChangedOpenApi changedOpenApi = OpenApiCompare.fromLocations(OPENAPI_DOC2, OPENAPI_DOC2);
+        assertThat(changedOpenApi.isUnchanged()).isTrue();
+    }
+}
