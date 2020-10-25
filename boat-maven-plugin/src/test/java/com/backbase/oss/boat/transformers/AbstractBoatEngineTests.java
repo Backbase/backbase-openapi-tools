ddf7cc9650897f2508609a07cd00ce352ed853c5
Bart Veenstra <bartv@backbase.com>
2020-10-25 12:16:19 +0100

    Moved generator to boat-scaffold and out of the boat-maven-plugin so it can be used by boat-terminal (the CLI) as well.

---
+++ boat-maven-plugin/src/test/java/com/backbase/oss/boat/transformers/AbstractBoatEngineTests.java
@@ -0,0 +1,32 @@
+package com.backbase.oss.boat.transformers;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AbstractBoatEngineTests {
+
+
+    protected File getFile(String name) {
+        URL resource = getClass().getResource(name);
+        assert resource != null;
+        return new File(resource.getFile());
+    }
+
+    protected void writeOutput(String yaml, String fileName) throws IOException {
+        File directory = new File("target");
+        directory.mkdirs();
+        File file = new File(directory, fileName);
+        file.delete();
+        file.createNewFile();
+        Path path = file.toPath();
+        log.info("Writing output to: {}", path);
+        Files.write(path, yaml.getBytes());
+
+
+    }
+}
