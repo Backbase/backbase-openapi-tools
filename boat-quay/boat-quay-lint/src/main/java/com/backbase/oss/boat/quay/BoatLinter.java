898c3bbd7b0f5f555f9d41ff80ea8dca8b43a35c
Bart Veenstra <bartv@backbase.com>
2020-10-29 15:52:12 +0100

    Rewrite documentation base url, prep to make it configurable Added UniqueOperationIdRule.kt Added SpectralTests unit tests (WIP)

--- boat-quay/boat-quay-lint/src/main/java/com/backbase/oss/boat/quay/BoatLinter.java
+++ boat-quay/boat-quay-lint/src/main/java/com/backbase/oss/boat/quay/BoatLinter.java
@@ -1,17 +1,24 @@
 package com.backbase.oss.boat.quay;
 
+import com.fasterxml.jackson.core.JsonPointer;
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import kotlin.ranges.IntRange;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zalando.zally.core.ApiValidator;
 import org.zalando.zally.core.Result;
 import org.zalando.zally.core.RulesPolicy;
+import org.zalando.zally.rule.api.Severity;
 
 public class BoatLinter {
 
     private final Logger log = LoggerFactory.getLogger(BoatLinter.class);
     private final ApiValidator validator;
+
+    private URI documentationBaseUrl = URI.create("https://backbase.github.io/backbase-openapi-tools/rules.md");
 
     public BoatLinter(ApiValidator validator) {
         this.validator = validator;
@@ -19,7 +26,27 @@ public class BoatLinter {
 
     public List<Result> lint(String openApiContent) {
         RulesPolicy rulesPolicy = new RulesPolicy(Arrays.asList("219", "105","M008", " M009", " M010", " M011", " H001", " H002", " S005", " S006", " S007"));
-        return validator.validate(openApiContent, rulesPolicy, null);
+        List<Result> validate = validator.validate(openApiContent, rulesPolicy, null);
+
+        return validate.stream().map(this::transformResult).collect(Collectors.toList());
+    }
+
+    private Result transformResult(Result result) {
+
+        String id = result.getId();
+        String title = result.getTitle();
+        String description = result.getDescription();
+        Severity violationType = result.getViolationType();
+        JsonPointer pointer = result.getPointer();
+        IntRange lines = result.getLines();
+
+        String heading= id + ":" + title;
+        String ref = heading.toLowerCase().replaceAll("[^a-z0-9]+","-");
+
+        URI resolve = documentationBaseUrl.resolve("#" + ref);
+
+        return new Result(id, resolve, title, description, violationType, pointer, lines);
+
     }
 
 }
---
+++ boat-quay/boat-quay-rules/readme.md
@@ -0,0 +1,18 @@
+Boat QUAY rules are based on the one set by Zalando and Spectral.
+
+Below is a summary on how these compare:
+
+https://meta.stoplight.io/docs/spectral/docs/reference/openapi-rules.md#operation-2xx-response
+
+Covered by:
+
+https://opensource.zalando.com/restful-api-guidelines/#150
+https://opensource.zalando.com/restful-api-guidelines/#151
+
+
+
+https://meta.stoplight.io/docs/spectral/docs/reference/openapi-rules.md#operation-operationid-unique
+
+Covered by:
+UniqueOperationIdRule
+
