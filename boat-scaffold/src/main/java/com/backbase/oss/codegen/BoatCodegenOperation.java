dce6dfbd21c189ce604327b8532e9cb04c7af3ae
Bart Veenstra <bartv@backbase.com>
2020-11-09 12:41:26 +0100

    Fix

--- boat-scaffold/src/main/java/com/backbase/oss/codegen/BoatCodegenOperation.java
+++ boat-scaffold/src/main/java/com/backbase/oss/codegen/BoatCodegenOperation.java
@@ -1,9 +1,6 @@
 package com.backbase.oss.codegen;
 
-import com.fasterxml.jackson.databind.util.BeanUtil;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Map;
-import org.apache.commons.beanutils.BeanUtils;
 import org.openapitools.codegen.CodegenOperation;
 
 public class BoatCodegenOperation extends CodegenOperation {
@@ -11,6 +8,5 @@ public class BoatCodegenOperation extends CodegenOperation {
 
     BoatCodegenOperation(CodegenOperation codegenOperation) throws InvocationTargetException, IllegalAccessException {
         super();
-        BeanUtils.copyProperties(this, codegenOperation);
     }
 }
