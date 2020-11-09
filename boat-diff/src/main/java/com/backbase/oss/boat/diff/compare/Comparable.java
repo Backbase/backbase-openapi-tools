334ee750c7ee6cd3682951ca814579ba85ed898a
Bart Veenstra <bartv@backbase.com>
2020-11-09 12:27:55 +0100

    Added examples column in html2 docs Example formatting for query string parameters

--- boat-diff/src/main/java/com/backbase/oss/boat/diff/compare/Comparable.java
+++
@@ -1,6 +0,0 @@
-package com.backbase.oss.boat.diff.compare;
-
-public interface Comparable<T> {
-
-    boolean compare(T left, T right);
-}
---
+++ boat-scaffold/src/main/java/com/backbase/oss/codegen/BoatCodegenProperty.java
@@ -0,0 +1,6 @@
+package com.backbase.oss.codegen;
+
+import org.openapitools.codegen.CodegenProperty;
+
+public class BoatCodegenProperty extends CodegenProperty {
+}
