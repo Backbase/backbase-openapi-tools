74adb8d0c258f7bf0bc328d5585ffe005b501b20
Bart Veenstra <bartv@backbase.com>
2020-11-11 09:54:30 +0100

    Reduce smellyness

--- boat-scaffold/src/main/java/com/backbase/oss/codegen/BoatExample.java
+++ boat-scaffold/src/main/java/com/backbase/oss/codegen/BoatExample.java
@@ -2,11 +2,14 @@ package com.backbase.oss.codegen;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.models.examples.Example;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 @Data
+@Slf4j
 public class BoatExample {
 
     private static ObjectMapper objectMapper = new ObjectMapper();
@@ -26,15 +29,16 @@ public class BoatExample {
     }
 
     public String getPrettyPrintValue() {
-        String s = null;
-        try {
-            s = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(example.getValue());
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
+        if (example.getValue() instanceof JsonNode) {
+            try {
+                return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(example.getValue());
+            } catch (JsonProcessingException e) {
+                log.error("Failed to pretty print example: {}", example.getValue(), e);
+                return example.getValue().toString();
+            }
+        } else {
+            return example.getValue().toString();
         }
-        return s;
-
     }
-
 
 }
