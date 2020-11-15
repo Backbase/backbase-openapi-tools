b742a1d5a03b7e826abef9c82d3894d056480fbd
pappy <pa314159@users.noreply.github.com>
2020-11-15 14:16:50 +0100

    modernised the boat terminal

--- boat-terminal/src/test/java/com/backbase/oss/boat/BoatTerminalTest.java
+++ boat-terminal/src/test/java/com/backbase/oss/boat/BoatTerminalTest.java
@@ -1,14 +1,14 @@
 package com.backbase.oss.boat;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 import org.junit.Test;
 
 public class BoatTerminalTest {
 
-    @Test(expected = Test.None.class)
+    @Test
     public void testCLI() {
-        BoatTerminal.main(new String[]{});
-
-
+        assertThat(BoatTerminal.run(new String[] {"-V"}), is(0));
     }
-
 }
