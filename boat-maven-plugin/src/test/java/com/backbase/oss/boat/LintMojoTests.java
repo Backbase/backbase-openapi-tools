6bb7aa5a4e6f94bcb62ed5861bd0fca6637717a5
Bart Veenstra <bartv@backbase.com>
2020-11-16 12:30:17 +0100

    Improve code coverage

--- boat-maven-plugin/src/test/java/com/backbase/oss/boat/LintMojoTests.java
+++ boat-maven-plugin/src/test/java/com/backbase/oss/boat/LintMojoTests.java
@@ -1,22 +1,37 @@
 package com.backbase.oss.boat;
 
 import java.io.File;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.junit.Assert;
+import org.apache.maven.plugin.MojoFailureException;
 import org.junit.Test;
 
 @Slf4j
 @SuppressWarnings("java:S2699")
 public class LintMojoTests {
 
-    @SneakyThrows
     @Test
-    public void testNonBreakingChange() {
+    public void testLintFile() throws MojoFailureException, MojoExecutionException {
         LintMojo lintMojo = new LintMojo();
         lintMojo.setInput(getFile("/oas-examples/petstore.yaml"));
         lintMojo.setFailOnWarning(false);
+        lintMojo.execute();
+    }
+
+
+    @Test
+    public void testLintDirectory() throws MojoFailureException, MojoExecutionException {
+        LintMojo lintMojo = new LintMojo();
+        lintMojo.setInput(getFile("/oas-examples/"));
+        lintMojo.setFailOnWarning(false);
+        lintMojo.execute();
+    }
+
+    @Test(expected = MojoExecutionException.class)
+    public void testFailOnWarning() throws MojoFailureException, MojoExecutionException {
+        LintMojo lintMojo = new LintMojo();
+        lintMojo.setInput(getFile("/oas-examples/petstore.yaml"));
+        lintMojo.setFailOnWarning(true);
         lintMojo.execute();
     }
 
--- boat-quay/boat-quay-lint/src/main/java/com/backbase/oss/boat/quay/configuration/RulesValidatorConfiguration.java
+++ boat-quay/boat-quay-lint/src/main/java/com/backbase/oss/boat/quay/configuration/RulesValidatorConfiguration.java
@@ -1,25 +1,19 @@
 package com.backbase.oss.boat.quay.configuration;
 
-import com.backbase.oss.boat.quay.BoatLinter;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.ConfigParseOptions;
 import com.typesafe.config.impl.ConfigImpl;
 import com.typesafe.config.impl.Parseable;
-import java.util.Set;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.zalando.zally.core.ApiValidator;
 import org.zalando.zally.core.CompositeRulesValidator;
 import org.zalando.zally.core.ContextRulesValidator;
 import org.zalando.zally.core.DefaultContextFactory;
 import org.zalando.zally.core.JsonRulesValidator;
-import org.zalando.zally.core.RuleProcessor;
 import org.zalando.zally.core.RulesManager;
 
 public class RulesValidatorConfiguration {
 
-    private final Logger log = LoggerFactory.getLogger(BoatLinter.class);
 
     public Config config(String ruleSetFile) {
         return defaultReference(this.getClass().getClassLoader(), ruleSetFile)
