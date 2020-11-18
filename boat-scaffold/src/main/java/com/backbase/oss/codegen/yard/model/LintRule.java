d8f6f4ab168134c521cb5d5e351c796875aab3ee
Bart Veenstra <bartv@backbase.com>
2020-11-18 12:35:40 +0100

    Set Lint Rule in Portal

---
+++ boat-scaffold/src/main/java/com/backbase/oss/codegen/yard/model/LintRule.java
@@ -0,0 +1,11 @@
+package com.backbase.oss.codegen.yard.model;
+
+import org.zalando.zally.rule.api.Severity;
+
+public class LintRule {
+
+    private String id;
+    private Severity overrideSeverity;
+    private boolean ignore;
+
+}
