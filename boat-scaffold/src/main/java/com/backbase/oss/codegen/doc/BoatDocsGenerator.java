b6190604c33e20dc4014f1470ac60195335de66f
Bart Veenstra <bartv@backbase.com>
2020-11-19 11:44:26 +0100

    Ensure single tags

--- boat-scaffold/src/main/java/com/backbase/oss/codegen/doc/BoatDocsGenerator.java
+++ boat-scaffold/src/main/java/com/backbase/oss/codegen/doc/BoatDocsGenerator.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import io.swagger.v3.core.util.Json;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.parameters.Parameter;
@@ -16,7 +15,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
@@ -59,33 +57,63 @@ public class BoatDocsGenerator extends org.openapitools.codegen.languages.Static
     public void preprocessOpenAPI(OpenAPI openAPI) {
         super.preprocessOpenAPI(openAPI);
 
-        // Add responses to additonal properties
-        // Should process responses and requests to convert them into code gen models.
-        if(openAPI.getComponents().getResponses()!=null) {
-
+        // Add responses to additional properties
+        if (openAPI.getComponents().getResponses() != null) {
             additionalProperties.put("responses", openAPI.getComponents().getResponses().entrySet().stream()
-                .map(codeResponse -> {
-                    String responseCode = codeResponse.getKey();
-                    // try to resolve response code from key. otherwise use default
-                    responseCode = responseCode.replaceAll("\\D+","");
-                    if(responseCode.length()!=3) {
-                        responseCode = "default";
-                    }
-
-                    ApiResponse response = codeResponse.getValue();
-                    return fromResponse(responseCode, response);
-                }).collect(Collectors.toList()));
+                .map(this::mapCodegenResponse)
+                .collect(Collectors.toList()));
         }
 
-        if(openAPI.getComponents().getParameters()!=null) {
+        // Add parameters to additonal properties
+        if (openAPI.getComponents().getParameters() != null) {
             Set<String> imports = new HashSet<>();
             additionalProperties.put("parameters", openAPI.getComponents().getParameters().entrySet().stream()
-            .map(nameParameter -> {
-                String name = nameParameter.getKey();
-                Parameter parameter = nameParameter.getValue();
-                return fromParameter(parameter, imports);
-            }).collect(Collectors.toList()));
+                .map(nameParameter -> mapComponentParameter(imports, nameParameter))
+                .collect(Collectors.toList()));
         }
+
+        // Add requests to addtional properties
+        if (openAPI.getComponents().getRequestBodies() != null) {
+            Set<String> imports = new HashSet<>();
+            additionalProperties.put("requestBodies", openAPI.getComponents().getRequestBodies().entrySet().stream()
+                .map(namedRequestBody -> mapComponentRequestBody(imports, namedRequestBody))
+                .collect(Collectors.toList()));
+        }
+
+        if (openAPI.getPaths() != null)
+            // Ensure single tags for operations
+            openAPI.getPaths().forEach((path, pathItem) -> {
+                pathItem.readOperations().forEach(operation -> {
+                    if (operation.getTags() != null && operation.getTags().size() > 1) {
+                        String tag = operation.getTags().get(operation.getTags().size() -1 );
+                        log.warn("Operation: {} contains multiple tags {} which hinders rendering documentation. Rep" +
+                            "lacing it with a single tag: {}", operation.getOperationId(), operation.getTags(), tag);
+                        operation.tags(Collections.singletonList(tag));
+                    }
+                });
+            });
+    }
+
+    private CodegenParameter mapComponentRequestBody(Set<String> imports, java.util.Map.Entry<String, RequestBody> namedRequestBody) {
+        String name = namedRequestBody.getKey();
+        RequestBody requestBody = namedRequestBody.getValue();
+        return fromRequestBody(requestBody, imports, name);
+    }
+
+    private CodegenParameter mapComponentParameter(Set<String> imports, java.util.Map.Entry<String, Parameter> nameParameter) {
+        Parameter parameter = nameParameter.getValue();
+        return fromParameter(parameter, imports);
+    }
+
+    private CodegenResponse mapCodegenResponse(java.util.Map.Entry<String, ApiResponse> codeResponse) {
+        String responseCode = codeResponse.getKey();
+        // try to resolve response code from key. otherwise use default
+        responseCode = responseCode.replaceAll("\\D+", "");
+        if (responseCode.length() != 3) {
+            responseCode = "default";
+        }
+        ApiResponse response = codeResponse.getValue();
+        return fromResponse(responseCode, response);
     }
 
 
