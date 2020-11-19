e20f78e7597136ac85968985ad7b7bb6c6698c39
Bart Veenstra <bartv@backbase.com>
2020-11-19 16:12:19 +0100

    Create Boat Linting Model UI for showing Lint Results WIP

--- boat-quay/boat-quay-lint/src/main/java/com/backbase/oss/boat/quay/BoatLinter.java
+++ boat-quay/boat-quay-lint/src/main/java/com/backbase/oss/boat/quay/BoatLinter.java
@@ -1,54 +1,109 @@
 package com.backbase.oss.boat.quay;
 
-import com.fasterxml.jackson.core.JsonPointer;
+import com.backbase.oss.boat.loader.OpenAPILoader;
+import com.backbase.oss.boat.quay.configuration.RulesValidatorConfiguration;
+import com.backbase.oss.boat.quay.model.BoatLintReport;
+import com.backbase.oss.boat.quay.model.BoatLintRule;
+import com.backbase.oss.boat.quay.model.BoatViolation;
+import com.typesafe.config.Config;
+import io.swagger.v3.oas.models.OpenAPI;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
-import kotlin.ranges.IntRange;
+import org.jetbrains.annotations.NotNull;
 import org.zalando.zally.core.ApiValidator;
+import org.zalando.zally.core.DefaultContextFactory;
 import org.zalando.zally.core.Result;
+import org.zalando.zally.core.RulesManager;
 import org.zalando.zally.core.RulesPolicy;
-import org.zalando.zally.rule.api.Severity;
+import org.zalando.zally.rule.api.Rule;
+import org.zalando.zally.rule.api.RuleSet;
 
 public class BoatLinter {
 
     private final ApiValidator validator;
 
     private final URI documentationBaseUrl = URI.create("https://backbase.github.io/backbase-openapi-tools/rules.md");
+    private final RulesManager rulesManager;
+    private final RulesPolicy rulesPolicy;
+    private final Map<String, BoatLintRule> availableRules;
 
-    public BoatLinter(ApiValidator validator) {
-        this.validator = validator;
+    public BoatLinter() {
+        this(new String[]{});
     }
 
-    public List<Result> lint(String openApiContent) {
-        return lint(openApiContent, "219", "105", "M008", " M009", " M010", " M011", " H001", " H002", " S005", " S006", " S007");
+    public BoatLinter(String... ignoreRules) {
+
+        RulesValidatorConfiguration rulesValidatorConfiguration = new RulesValidatorConfiguration();
+        Config config = rulesValidatorConfiguration.config("boat.conf");
+        this.rulesManager = rulesValidatorConfiguration.rulesManager(config);
+        this.rulesPolicy = new RulesPolicy(Arrays.asList(ignoreRules));
+        this.validator = rulesValidatorConfiguration.apiValidator(rulesManager, new DefaultContextFactory());
+        this.availableRules = mapAvailableRules();
     }
 
-    public List<Result> lint(String openApiContent, String... ignoreRules) {
-        RulesPolicy rulesPolicy = new RulesPolicy(Arrays.asList(ignoreRules));
+    public BoatLintReport lint(String openApiContent) {
         List<Result> validate = validator.validate(openApiContent, rulesPolicy, null);
+        List<BoatViolation> violations = validate.stream()
+            .map(this::transformResult)
+            .collect(Collectors.toList());
+        OpenAPI openAPI = OpenAPILoader.parse(openApiContent);
 
-        return validate.stream().map(this::transformResult).collect(Collectors.toList());
+        BoatLintReport boatLintReport = new BoatLintReport();
+        boatLintReport.setOpenApi(openApiContent);
+        boatLintReport.setAvailableRules(getAvailableRules());
+        boatLintReport.setViolations(violations);
+        boatLintReport.setTitle(openAPI.getInfo().getTitle());
+        boatLintReport.setVersion(openAPI.getInfo().getVersion());
+
+        return boatLintReport;
+
+
     }
 
-    private Result transformResult(Result result) {
+    private BoatViolation transformResult(Result result) {
 
-        String id = result.getId();
-        String title = result.getTitle();
-        String description = result.getDescription();
-        Severity violationType = result.getViolationType();
-        JsonPointer pointer = result.getPointer();
-        IntRange lines = result.getLines();
+        BoatViolation violation = new BoatViolation();
+        violation.setLintRule(availableRules.get(result.getId()));
+        violation.setDescription(result.getDescription());
+        violation.setPointer(result.getPointer());
+        violation.setLines(result.getLines());
+        violation.setSeverity(result.getViolationType());
+        return violation;
+    }
 
+    @NotNull
+    private URI getUri(String id, String title) {
         String heading = id + ":" + title;
         String ref = heading.toLowerCase().replaceAll("[^a-z0-9]+", "-");
-
-        URI resolve = documentationBaseUrl.resolve("#" + ref);
-
-        return new Result(id, resolve, title, description, violationType, pointer, lines);
-
+        return documentationBaseUrl.resolve("#" + ref);
     }
 
 
+    private Map<String, BoatLintRule> mapAvailableRules() {
+        Map<String, BoatLintRule> rules = new LinkedHashMap<>();
+
+        rulesManager.rules(rulesPolicy)
+            .forEach(ruleDetails -> {
+                BoatLintRule boatLintRule = new BoatLintRule();
+                Rule rule = ruleDetails.getRule();
+                RuleSet ruleSet = ruleDetails.getRuleSet();
+                boatLintRule.setId(rule.id());
+                boatLintRule.setUrl(getUri(rule.id(), rule.title()));
+                boatLintRule.setTitle(rule.title());
+                boatLintRule.setSeverity(rule.severity());
+                boatLintRule.setIgnored(false);
+                boatLintRule.setRuleSet(ruleSet.getId());
+                rules.put(rule.id(), boatLintRule);
+            });
+        return rules;
+    }
+
+    public List<BoatLintRule> getAvailableRules() {
+        return new ArrayList<>(availableRules.values());
+    }
 }
