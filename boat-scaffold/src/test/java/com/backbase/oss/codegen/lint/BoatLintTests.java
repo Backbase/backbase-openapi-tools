2918debe5e35e53e2085889d298d012fb191d725
Bart Veenstra <bartv@backbase.com>
2020-11-19 10:33:37 +0100

    Add lint report WIP

---
+++ boat-scaffold/src/test/java/com/backbase/oss/codegen/lint/BoatLintTests.java
@@ -0,0 +1,31 @@
+package com.backbase.oss.codegen.lint;
+
+import com.backbase.oss.codegen.yard.BoatYardConfig;
+import com.backbase.oss.codegen.yard.BoatYardGenerator;
+import java.io.File;
+import java.net.URL;
+import org.junit.Test;
+
+public class BoatLintTests {
+
+    @Test
+    public void testBoatLin() {
+
+        File input = getFile("/oas-examples/petstore.yaml");
+        File output = new File("target/boat-lint");
+
+        BoatLintConfig config = new BoatLintConfig();
+        config.setInputSpec(input.getAbsolutePath());
+        config.setOutputDir(output.getAbsolutePath());
+
+        config.setTemplateDir("boat-yard");
+
+        new BoatLintGenerator(config).generate();
+    }
+
+    protected File getFile(String name) {
+        URL resource = getClass().getResource(name);
+        assert resource != null;
+        return new File(resource.getFile());
+    }
+}
