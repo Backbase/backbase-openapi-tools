ee2c84af657da514844a521d1ed95cbbbcae9732
Sophie Janssens <sophiej@backbase.com>
2020-12-01 10:05:55 +0100

    Tests data

---
+++ boat-engine/src/test/java/com/backbase/oss/boat/ConversionTests.java
@@ -0,0 +1,18 @@
+package com.backbase.oss.boat;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.media.Schema;
+import org.junit.Test;
+
+import java.util.Map;
+
+public class ConversionTests extends AbstractBoatEngineTests{
+
+    @Test
+    public void testXmlSchemaToOpenApi(){
+      //  Map<String, Schema> testSchemas =
+      //  XmlSchemaToOpenApi.convert("cards","Content", new Components().schemas())
+
+    }
+
+}
