d08cfe29acfd5141663057a9945ff478428c879a
Sophie Janssens <sophiej@backbase.com>
2020-12-07 13:44:34 +0100

    added unit test

--- boat-engine/src/test/java/com/backbase/oss/boat/ResolverTests.java
+++ boat-engine/src/test/java/com/backbase/oss/boat/ResolverTests.java
@@ -3,9 +3,16 @@ package com.backbase.oss.boat;
 import com.backbase.oss.boat.loader.OpenAPILoader;
 import com.backbase.oss.boat.loader.OpenAPILoaderException;
 import java.io.File;
+import java.util.ArrayList;
+
+import com.backbase.oss.boat.transformers.bundler.BoatCache;
+import com.backbase.oss.boat.transformers.bundler.ExamplesProcessor;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.examples.Example;
+import io.swagger.v3.parser.models.RefFormat;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ResolverTests extends AbstractBoatEngineTestBase {
 
@@ -14,4 +21,17 @@ public class ResolverTests extends AbstractBoatEngineTestBase {
         assertThrows(OpenAPILoaderException.class, () ->
             OpenAPILoader.load(new File("invalidOpenAPI.yaml"), false));
     }
+
+    @Test
+    public void testBoatCache() throws OpenAPILoaderException {
+        OpenAPI remoteReference = OpenAPILoader.load(getFile("/openapi/bundler-examples-test-api/openapi.yaml"));
+        BoatCache boatCache = new BoatCache(remoteReference, new ArrayList<>(), "src/test/resources/openapi/bundler-examples-test-api/openapi.yaml",new ExamplesProcessor(remoteReference,"src/test/resources/openapi/bundler-examples-test-api/openapi.yaml"));
+
+        assertEquals("{\n" + "  \"id\": \"11bd3ca6-5a26-4d97-a3f1-c59df4d6c02f\",\n" + "  \"name\": \"Alex\",\n" + "  \"role\": \"Manager\",\n" + "  \"rank\": 12,\n" + "  \"optional-param\": \"012\"\n" + "}"
+                ,boatCache.loadRef("./examples/user-post-response.json#/element", RefFormat.RELATIVE, Example.class).getValue().toString());
+    }
+
+
+
+
 }
--- boat-scaffold/src/test/java/com/backbase/oss/codegen/lint/BoatLintTests.java
+++ boat-scaffold/src/test/java/com/backbase/oss/codegen/lint/BoatLintTests.java
@@ -3,13 +3,20 @@ package com.backbase.oss.codegen.lint;
 import com.backbase.oss.codegen.yard.BoatYardConfig;
 import com.backbase.oss.codegen.yard.BoatYardGenerator;
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 import java.net.URL;
+import java.nio.file.*;
+
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BoatLintTests {
 
     @Test
-    public void testBoatLint() {
+    public void testBoatLint() throws IOException {
 
         File input = getFile("/oas-examples/petstore.yaml");
         File output = new File("target/boat-lint");
@@ -21,6 +28,15 @@ public class BoatLintTests {
         config.setTemplateDir("boat-lint");
 
         new BoatLintGenerator(config).generate();
+
+        String[] actualDirectorySorted =output.list();
+        Arrays.sort(actualDirectorySorted);
+        String[] expectedDirectory= {"backbase-logo.svg","css","index.html","js"};
+        assertArrayEquals(expectedDirectory,actualDirectorySorted);
+
+        File index = new File("target/boat-lint/index.html");
+        String generated = String.join( " ", Files.readAllLines(Paths.get(index.getPath())));
+        assertTrue(generated.contains("<title>BOAT Lint Report - Swagger Petstore</title>"));
     }
 
     protected File getFile(String name) {
