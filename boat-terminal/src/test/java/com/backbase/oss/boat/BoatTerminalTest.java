8a638a14d638f045c86b6519f5e010e99d0f601e
Sophie Janssens <sophiej@backbase.com>
2020-12-08 14:30:43 +0100

    added error handling tests

--- boat-terminal/src/test/java/com/backbase/oss/boat/BoatTerminalTest.java
+++ boat-terminal/src/test/java/com/backbase/oss/boat/BoatTerminalTest.java
@@ -1,9 +1,11 @@
 package com.backbase.oss.boat;
 
+import ch.qos.logback.classic.Level;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BoatTerminalTest {
 
@@ -11,4 +13,24 @@ class BoatTerminalTest {
     void testCLI() {
         assertThat(BoatTerminal.run(new String[]{"-V"}), is(0));
     }
+
+    @Test
+    void testVerbose(){
+        boolean[] testVerbose = {true,false};
+        BoatTerminal boatTerminal = new BoatTerminal();
+        boatTerminal.setVerbose(testVerbose);
+        assertEquals(Level.DEBUG,boatTerminal.getRootLevel());
+
+        testVerbose = new boolean[]{true};
+
+        boatTerminal.setVerbose(testVerbose);
+        assertEquals(Level.INFO,boatTerminal.getRootLevel());
+
+        testVerbose = new boolean[]{true,true,true};
+        boatTerminal.setVerbose(testVerbose);
+        assertEquals(Level.TRACE,boatTerminal.getRootLevel());
+
+    }
+
+
 }
