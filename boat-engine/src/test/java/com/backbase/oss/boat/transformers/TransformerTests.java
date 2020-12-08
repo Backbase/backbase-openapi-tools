0d6a75d4411f7570179e8023d2d7638949961ee0
Sophie Janssens <sophiej@backbase.com>
2020-12-08 11:11:26 +0100

    removed redundant test

--- boat-engine/src/test/java/com/backbase/oss/boat/transformers/TransformerTests.java
+++ boat-engine/src/test/java/com/backbase/oss/boat/transformers/TransformerTests.java
@@ -22,13 +22,5 @@ public class TransformerTests {
 
 
 
-//    @Test
-//    public void testUnAliasExternalRef() throws OpenAPILoaderException {
-//        // should no longer contain refs
-//        File input = new File("src/test/resources/openapi/un-alias-api/openapi.yaml");
-//        OpenAPI openAPI = OpenAPILoader.load(input);
-//        new UnAliasTransformer().transform(openAPI, Collections.EMPTY_MAP);
-//        assertTrue(!openAPI.getOpenapi().contains("$ref"));
-//    }
 
 }
