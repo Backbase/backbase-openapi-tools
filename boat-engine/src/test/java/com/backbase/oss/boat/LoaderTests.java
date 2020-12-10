1ca8ee735a09eba40b3572a7b94d6a1e16ceb439
Sophie Janssens <sophiej@backbase.com>
2020-12-10 14:29:57 +0100

    xml schema test

--- boat-engine/src/test/java/com/backbase/oss/boat/LoaderTests.java
+++ boat-engine/src/test/java/com/backbase/oss/boat/LoaderTests.java
@@ -1,30 +1,30 @@
 package com.backbase.oss.boat;
 
-import com.backbase.oss.boat.loader.CachingResourceLoader;
-import com.backbase.oss.boat.loader.RamlLoaderException;
-import com.backbase.oss.boat.loader.RamlResourceLoader;
+import com.backbase.oss.boat.loader.*;
+
 import java.io.File;
 import org.junit.jupiter.api.Test;
+import org.raml.v2.api.loader.ResourceUriCallback;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LoaderTests extends AbstractBoatEngineTestBase {
 
     @Test
-    void testRamlResourceLoaderExceptions() {
+    void testRamlResourceLoaderException() {
         File root = new File("src/test/resources/openapi/error-catching");
         File base = new File("src/test/resources/openapi/error-catching");
         String resourceName = "empty-directory";
         RamlResourceLoader ramlResourceLoader = new RamlResourceLoader(base, root);
         assertThrows(RamlLoaderException.class, () -> ramlResourceLoader.fetchResource(resourceName));
 
-         root = new File("src/test/resources/openapi/error-catching/ramlLoaderEception");
-         base = new File("src/test/resources/openapi/error-catching");
+
+    }
 
 
-
-
-
+    @Test
+    void testOpenApiLoader() {
+        assertThrows(OpenAPILoaderException.class,()->OpenAPILoader.load("src/test/resources/openapi/error-catching/empty-directory"));
     }
 
 
@@ -36,6 +36,8 @@ class LoaderTests extends AbstractBoatEngineTestBase {
         String resourceName = "empty-directory";
         CachingResourceLoader cachingResourceLoader = new CachingResourceLoader(new RamlResourceLoader(base, root));
         assertThrows(RamlLoaderException.class, () -> cachingResourceLoader.fetchResource(resourceName));
+
+
     }
 
 
