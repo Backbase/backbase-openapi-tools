7a95c8b85aea6b9d459625e179767c07b6690f1a
Fredy Sierra <fredy@backbase.com>
2020-12-15 12:05:13 +0100

    feat: Add Info description checker

--- boat-quay/boat-quay-rules/src/test/kotlin/com/backbase/oss/boat/quay/ruleset/InfoBlockDescriptionCheckerTest.kt
+++ boat-quay/boat-quay-rules/src/test/kotlin/com/backbase/oss/boat/quay/ruleset/InfoBlockDescriptionCheckerTest.kt
@@ -5,13 +5,52 @@ import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Test
 import org.zalando.zally.core.DefaultContextFactory
 
-class InfoBlockTitleCheckerTest {
+class InfoBlockDescriptionCheckerTest {
 
-    private val cut = InfoBlockTitleChecker()
+    private val cut = InfoBlockDescriptionChecker()
 
 
     @Test
-    fun `correct value for title`() {
+    fun `correct value for description`() {
+        @Language("YAML")
+        val context = DefaultContextFactory().getOpenApiContext(
+                """
+            openapi: 3.0.3
+            info:
+              title: Thing API
+              description: This is a meaningful description for API
+              version: 1.0.0
+            """.trimIndent()
+        )
+
+        val violations = cut.validate(context)
+
+        ZallyAssertions
+                .assertThat(violations)
+                .isEmpty()
+    }
+
+    @Test
+    fun `incorrect value for description max length`() {
+        @Language("YAML")
+        val context = DefaultContextFactory().getOpenApiContext(
+                """
+            openapi: 3.0.3
+            info:
+              title: Thing API
+              description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus placerat justo. Mauris eget tellus non ante interdum lacinia. Sed hendrerit justo.
+            """.trimIndent()
+        )
+
+        val violations = cut.validate(context)
+
+        ZallyAssertions
+                .assertThat(violations)
+                .isNotEmpty
+    }
+
+    @Test
+    fun `no value for description`() {
         @Language("YAML")
         val context = DefaultContextFactory().getOpenApiContext(
                 """
@@ -26,53 +65,18 @@ class InfoBlockTitleCheckerTest {
 
         ZallyAssertions
                 .assertThat(violations)
-                .isEmpty()
-    }
-
-    @Test
-    fun `incorrect value for title max length`() {
-        @Language("YAML")
-        val context = DefaultContextFactory().getOpenApiContext(
-                """
-            openapi: 3.0.3
-            info:
-              title: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
-            """.trimIndent()
-        )
-
-        val violations = cut.validate(context)
-
-        ZallyAssertions
-                .assertThat(violations)
                 .isNotEmpty
     }
 
     @Test
-    fun `no value for title`() {
+    fun `empty value for description`() {
         @Language("YAML")
         val context = DefaultContextFactory().getOpenApiContext(
                 """
             openapi: 3.0.3
             info:
-              version: 1.0.0
-            """.trimIndent()
-        )
-
-        val violations = cut.validate(context)
-
-        ZallyAssertions
-                .assertThat(violations)
-                .isNotEmpty
-    }
-
-    @Test
-    fun `empty value for title`() {
-        @Language("YAML")
-        val context = DefaultContextFactory().getOpenApiContext(
-                """
-            openapi: 3.0.3
-            info:
-              title: ''
+              title: Thing API
+              description: ''
               version: 1.0.0
             """.trimIndent()
         )
