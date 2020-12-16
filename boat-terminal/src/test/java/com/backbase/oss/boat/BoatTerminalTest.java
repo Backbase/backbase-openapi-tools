86e50fff58f0cbf136ad4ca8dfce6ede9cb110dd
Sophie Janssens <sophiej@backbase.com>
2020-12-16 11:59:10 +0000

    boat terminal test

--- boat-terminal/src/test/java/com/backbase/oss/boat/BoatTerminalTest.java
+++ boat-terminal/src/test/java/com/backbase/oss/boat/BoatTerminalTest.java
@@ -3,9 +3,14 @@ package com.backbase.oss.boat;
 import ch.qos.logback.classic.Level;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BoatTerminalTest {
 
@@ -13,6 +18,17 @@ class BoatTerminalTest {
     void testCLI() {
         assertThat(BoatTerminal.run(new String[]{"-V"}), is(0));
     }
+    @Test
+    void testCLIOptions() {
+        assertThat(BoatTerminal.run(new String[]{"-f=src/test/resources/raml-examples/backbase-wallet/presentation-client-api.raml","--directory=src/test/resources/raml-examples/converted","--output=src/test/resources/raml-examples/converted/openapi.yaml","--convert-examples=false"}), is(0));
+        File output = new File("src/test/resources/raml-examples/converted/openapi.yaml");
+
+        assertTrue(output.exists());
+
+        assertThat(BoatTerminal.run(new String[]{"--file=src/test/resources/raml-examples/backbase-wallet/presentation-client-api.raml","-d=src/test/resources/raml-examples/converted","-o=src/test/resources/raml-examples/converted/openapi.yaml","--convert-examples=false"}), is(0));
+        assertThat(BoatTerminal.run(new String[]{"--file=src/test/resources/raml-examples/backbase-wallet/presentation-client-api.raml","-d=src/test/resources/raml-examples/converted","-v"}),is(0));
+    }
+
 
     @Test
     void testVerbose(){
