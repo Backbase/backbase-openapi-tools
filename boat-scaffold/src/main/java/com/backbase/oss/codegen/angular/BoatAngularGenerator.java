91a1130dc3e9977bd6f221539bc1ccd9dbbe7229
Brenton Alker <brenton@backbase.com>
2020-12-18 14:14:49 +0000

    Apply `apiModulePrefix` to the `BASE_PATH` export
    The `apiModulePrefix` will be integrated into the name of the
    `BASE_PATH` export in UPPER_SNAKE_CASE format.

--- boat-scaffold/src/main/java/com/backbase/oss/codegen/angular/BoatAngularGenerator.java
+++ boat-scaffold/src/main/java/com/backbase/oss/codegen/angular/BoatAngularGenerator.java
@@ -41,6 +41,7 @@ import java.util.stream.Stream;
 
 import static org.apache.commons.lang3.StringUtils.capitalize;
 import static org.openapitools.codegen.utils.StringUtils.camelize;
+import static org.openapitools.codegen.utils.StringUtils.underscore;
 
 @Slf4j
 public class BoatAngularGenerator extends AbstractTypeScriptClientCodegen {
@@ -172,12 +173,12 @@ public class BoatAngularGenerator extends AbstractTypeScriptClientCodegen {
             additionalProperties.put("apiModuleClassName", value + "ApiModule");
             additionalProperties.put("configurationClassName", value + "Configuration");
             additionalProperties.put("configurationParametersInterfaceName", value + "ConfigurationParameters");
-            additionalProperties.put(API_MODULE_PREFIX, true);
+            additionalProperties.put("basePathVariableName", underscore(value).toUpperCase() + "_BASE_PATH");
         }, () -> {
             additionalProperties.put("apiModuleClassName", "ApiModule");
             additionalProperties.put("configurationClassName", "Configuration");
             additionalProperties.put("configurationParametersInterfaceName", "ConfigurationParameters");
-            additionalProperties.put(API_MODULE_PREFIX, false);
+            additionalProperties.put("basePathVariableName", "BASE_PATH");
         });
         processOpt(SERVICE_SUFFIX, value -> {
             serviceSuffix = value;
