7a32aed44c83be14a8d33a13bf6a6351889cd592
Fredy Sierra <fredy@backbase.com>
2020-12-21 17:25:04 +0100

    fix: Improve documentation when `mvn help:describe -DgroupId=com.backbase.oss -DartifactId=boat-maven-plugin` and adding lint documentation on README.md

--- boat-maven-plugin/src/main/java/com/backbase/oss/boat/AbstractLintMojo.java
+++ boat-maven-plugin/src/main/java/com/backbase/oss/boat/AbstractLintMojo.java
@@ -5,6 +5,7 @@ import com.backbase.oss.boat.quay.model.BoatLintReport;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.maven.plugin.AbstractMojo;
@@ -18,12 +19,28 @@ import org.apache.maven.plugins.annotations.Parameter;
  */
 public abstract class AbstractLintMojo extends AbstractMojo {
 
+    /**
+     * Input spec directory or file.
+     */
     @Parameter(name = "inputSpec", property = "inputSpec", required = true)
     protected File inputSpec;
 
+    /**
+     * Set this to <code>true</code> to fail in case a warning is found.
+     */
     @Parameter(name = "failOnWarning", defaultValue = "false")
     protected boolean failOnWarning;
 
+
+    /**
+     * Set this to <code>true</code> to show the list of ignored rules..
+     */
+    @Parameter(name = "showIgnoredRules", defaultValue = "false")
+    protected boolean showIgnoredRules;
+
+    /**
+     * List of rules ids which will be ignored.
+     */
     @Parameter(name = "ignoreRules")
     protected String[] ignoreRules = new String[]{"219", "105", "M008", " M009", " M010", " M011", " H001", " H002", " S005", " S006", " S007"};
 
@@ -51,9 +68,11 @@ public abstract class AbstractLintMojo extends AbstractMojo {
 
     private BoatLintReport lintOpenAPI(File inputFile) throws MojoExecutionException {
         try {
+            if(showIgnoredRules) {
+                log.info("These rules will be ignored: {}", Arrays.toString(ignoreRules));
+            }
             BoatLinter boatLinter = new BoatLinter(ignoreRules);
             return boatLinter.lint(inputFile);
-
         } catch (IOException e) {
             throw new MojoExecutionException("Error transforming OpenAPI: " + inputFile, e);
         }
