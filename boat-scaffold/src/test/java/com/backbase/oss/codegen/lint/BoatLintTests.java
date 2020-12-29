ba13a756b04191eb557811bb2abe5fefcc222907
Bart Veenstra <bartv@backbase.com>
2020-12-29 11:28:41 +0100

    Added more test cases

--- boat-scaffold/src/test/java/com/backbase/oss/codegen/lint/BoatLintTests.java
+++ boat-scaffold/src/test/java/com/backbase/oss/codegen/lint/BoatLintTests.java
@@ -1,17 +1,15 @@
 package com.backbase.oss.codegen.lint;
 
-import com.backbase.oss.codegen.yard.BoatYardConfig;
-import com.backbase.oss.codegen.yard.BoatYardGenerator;
+import com.backbase.oss.boat.loader.OpenAPILoaderException;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.net.URL;
-import java.nio.file.*;
-
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import org.jetbrains.annotations.NotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BoatLintTests {
 
@@ -19,6 +17,26 @@ class BoatLintTests {
     void testBoatLint() throws IOException {
 
         File input = getFile("/oas-examples/petstore.yaml");
+        File output = generate(input);
+
+        String[] actualDirectorySorted = output.list();
+        Arrays.sort(actualDirectorySorted);
+        String[] expectedDirectory = {"backbase-logo.svg", "css", "index.html", "js"};
+        assertArrayEquals(expectedDirectory, actualDirectorySorted);
+
+        File index = new File("target/boat-lint/index.html");
+        String generated = String.join(" ", Files.readAllLines(Paths.get(index.getPath())));
+        assertTrue(generated.contains("<title>BOAT Lint Report - Swagger Petstore</title>"));
+    }
+
+    @Test
+    void testUnreadable() {
+        File input = getFile("/oas-examples/unable-to-parse.yaml");
+        assertThrows(IllegalArgumentException.class, () -> generate(input));
+    }
+
+    @NotNull
+    private File generate(File input) {
         File output = new File("target/boat-lint");
 
         BoatLintConfig config = new BoatLintConfig();
@@ -28,16 +46,9 @@ class BoatLintTests {
         config.setTemplateDir("boat-lint");
 
         new BoatLintGenerator(config).generate();
-
-        String[] actualDirectorySorted =output.list();
-        Arrays.sort(actualDirectorySorted);
-        String[] expectedDirectory= {"backbase-logo.svg","css","index.html","js"};
-        assertArrayEquals(expectedDirectory,actualDirectorySorted);
-
-        File index = new File("target/boat-lint/index.html");
-        String generated = String.join( " ", Files.readAllLines(Paths.get(index.getPath())));
-        assertTrue(generated.contains("<title>BOAT Lint Report - Swagger Petstore</title>"));
+        return output;
     }
+
 
     protected File getFile(String name) {
         URL resource = getClass().getResource(name);
