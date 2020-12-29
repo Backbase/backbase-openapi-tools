e79d945ecbdf0541a280bfe02ffb8d8642522aae
Bart Veenstra <bartv@backbase.com>
2020-12-29 09:46:28 +0100

    Throw exceptons when parsing fails

--- boat-engine/src/main/java/com/backbase/oss/boat/loader/OpenAPILoaderException.java
+++ boat-engine/src/main/java/com/backbase/oss/boat/loader/OpenAPILoaderException.java
@@ -1,13 +1,28 @@
 package com.backbase.oss.boat.loader;
 
 
+import java.util.List;
+
 public class OpenAPILoaderException extends Exception {
 
-    public OpenAPILoaderException(String message) {
+    private final List<String> parseMessages;
+
+    public OpenAPILoaderException(String message, List<String> messages) {
         super(message);
+        this.parseMessages = messages;
     }
 
     public OpenAPILoaderException(String message, Throwable throwable) {
         super(message, throwable);
+        parseMessages = null;
+    }
+
+    public OpenAPILoaderException(String message) {
+        super(message);
+        parseMessages = null;
+    }
+
+    public List<String> getParseMessages() {
+        return parseMessages;
     }
 }
