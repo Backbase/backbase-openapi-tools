efd3c25fa1a6b7db586ab35894f208e8e6e00f29
Christopher Morgan <christopherm@backbase.com>
2021-01-18 12:04:25 +0000

    Fix building on windows.

--- boat-maven-plugin/src/main/java/com/backbase/oss/boat/GenerateMojo.java
+++ boat-maven-plugin/src/main/java/com/backbase/oss/boat/GenerateMojo.java
@@ -581,8 +581,12 @@ public class GenerateMojo extends AbstractMojo {
                 configurator.setRemoveOperationIdPrefix(removeOperationIdPrefix);
             }
 
-            if (isNotEmpty(inputSpec)) {
+            if (isValidURI(inputSpec)) {
                 configurator.setInputSpec(inputSpec);
+            } else if (inputSpecFile.exists()) {
+                configurator.setInputSpec(inputSpecFile.getAbsoluteFile().toURI().toString());
+            } else {
+                throw new MojoExecutionException(inputSpec + " is not a valid URI or file!");
             }
 
             if (isNotEmpty(gitHost)) {
@@ -1047,4 +1051,14 @@ public class GenerateMojo extends AbstractMojo {
             }
         }
     }
+
+    private static boolean isValidURI(String urlString) {
+        try {
+            URI uri = new URI(urlString);
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
+    }
+
 }
