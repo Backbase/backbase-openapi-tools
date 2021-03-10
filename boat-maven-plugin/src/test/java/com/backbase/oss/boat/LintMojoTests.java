0c269b11ca13606cf2f37036cdaac1169b46c207
Sophie Janssens <sophiej@backbase.com>
2021-03-10 16:57:42 +0000

    Fixed issues with client tests, covered by it tests so removed

--- boat-maven-plugin/src/test/java/com/backbase/oss/boat/LintMojoTests.java
+++ boat-maven-plugin/src/test/java/com/backbase/oss/boat/LintMojoTests.java
@@ -87,21 +87,7 @@ class LintMojoTests {
     }
 
 
-    @Test
-    void testExceptionsWithValidHttpCodeFile() {
-        LintMojo lintMojo = new LintMojo();
-        lintMojo.setInput(getFile("/oas-examples/petstore-backbase-httpcodes.yaml"));
-        assertDoesNotThrow(lintMojo::execute);
 
-    }
-    @Test
-    void testIgnoredExceptionsWithValidHttpCodeFile() {
-        LintMojo lintMojo = new LintMojo();
-        lintMojo.setInput(getFile("/oas-examples/petstore-invalid-http-codes.yaml"));
-        lintMojo.showIgnoredRules = true;
-        assertDoesNotThrow(lintMojo::execute);
-
-    }
 
 
 
