52d37402a4615d2c0c29d0edd2fb2549e6a1a5ca
Sophie Janssens <sophiej@backbase.com>
2021-03-17 16:02:11 +0000

    Fix: confusion with standard status codes rule, multiple checks seemed contradictory and unclear, now split up

--- boat-quay/boat-quay-rules/src/main/kotlin/com/backbase/oss/boat/quay/ruleset/UseBackbaseStandardHttpStatusCodesRule.kt
+++ boat-quay/boat-quay-rules/src/main/kotlin/com/backbase/oss/boat/quay/ruleset/UseBackbaseStandardHttpStatusCodesRule.kt
@@ -12,35 +12,7 @@ import io.swagger.v3.oas.models.PathItem
 )
 class UseBackbaseStandardHttpStatusCodesRule(config: Config) {
 
-        private val wellUnderstoodResponseCodesAndVerbs = config
-                .getConfig("${javaClass.simpleName}.well_understood")
-                .entrySet()
-                .map { (key, config) ->
-                    @Suppress("UNCHECKED_CAST")
-                    key to config.unwrapped() as List<String>
-                }
-                .toMap()
-
-        private val wellUnderstoodResponseCode = wellUnderstoodResponseCodesAndVerbs.keys
-
         private val standardResponseCodes = config.getStringList("${javaClass.simpleName}.standard")
-
-        /**
-         * Validate that well-understood HTTP response codes are used properly
-         * @param context the context to validate
-         * @return list of identified violations
-         */
-
-
-        @Check(severity = Severity.SHOULD)
-        fun checkWellUnderstoodResponseCodesUsage(context: Context): List<Violation> =
-                context.validateOperations { (method, operation) ->
-                    operation?.responses.orEmpty().filterNot { (status, _) ->
-                        isAllowed(method, status)
-                    }.map { (_, response) ->
-                        context.violation("Operations should use well-understood HTTP status codes", response)
-                    }
-                }
 
         /**
          * Validate that only standardized HTTP response codes are used
@@ -56,26 +28,5 @@ class UseBackbaseStandardHttpStatusCodesRule(config: Config) {
                         context.violation("$status is not a standardized response code", response)
                     }
                 }
-
-        /**
-         * Check that well-understood HTTP response codes are used
-         * @param context the context to validate
-         * @return list of identified violations
-         */
-        @Check(severity = Severity.SHOULD)
-        fun checkIfOnlyWellUnderstoodResponseCodesAreUsed(context: Context): List<Violation> =
-                context.validateOperations { (_, operation) ->
-                    operation?.responses.orEmpty().filterNot { (status, _) ->
-                        status in wellUnderstoodResponseCode
-                    }.map { (status, response) ->
-                        context.violation("$status is not a well-understood response code", response)
-                    }
-                }
-
-        private fun isAllowed(method: PathItem.HttpMethod, statusCode: String): Boolean {
-            val allowedMethods = wellUnderstoodResponseCodesAndVerbs[statusCode.toLowerCase()].orEmpty()
-            return allowedMethods.contains(method.name) || allowedMethods.contains("ALL")
-        }
-
 
 }
---
+++ boat-quay/boat-quay-rules/src/main/kotlin/com/backbase/oss/boat/quay/ruleset/UseBackbaseWellUnderstoodHttpStatusCodesRule.kt
@@ -0,0 +1,56 @@
+package com.backbase.oss.boat.quay.ruleset
+
+import com.typesafe.config.Config
+import org.zalando.zally.rule.api.*
+import io.swagger.v3.oas.models.PathItem
+
+@Rule(
+        ruleSet = BoatRuleSet::class,
+        id = "B012",
+        severity = Severity.SHOULD,
+        title = "Use Well Understood HTTP Status Codes"
+)
+class UseBackbaseWellUnderstoodHttpStatusCodesRule(config: Config) {
+
+        private val wellUnderstoodResponseCodesAndVerbs = config
+                .getConfig("${javaClass.simpleName}.well_understood")
+                .entrySet()
+                .map { (key, config) ->
+                    @Suppress("UNCHECKED_CAST")
+                    key to config.unwrapped() as List<String>
+                }
+                .toMap()
+
+        private val wellUnderstoodResponseCode = wellUnderstoodResponseCodesAndVerbs.keys
+
+
+        /**
+         * Validate that well-understood HTTP response codes are used properly
+         * @param context the context to validate
+         * @return list of identified violations
+         */
+
+        @Check(severity = Severity.SHOULD)
+        fun checkWellUnderstoodResponseCodesUsage(context: Context): List<Violation> =
+                context.validateOperations { (method, operation) ->
+                    operation?.responses.orEmpty().filterNot { (status, _) ->
+                        isAllowed(method, status)
+                    }.map { (status, response) ->
+                        if (wellUnderstoodResponseCode.contains(status)){
+                            context.violation("$status is not allowed for this method, incorrect use of well known code", response)
+                        }else{
+                            context.violation("$status is not a well understood status code, should use well-understood HTTP status codes", response)
+                        }
+
+                    }
+                }
+
+
+        private fun isAllowed(method: PathItem.HttpMethod, statusCode: String): Boolean {
+            val allowedMethods = wellUnderstoodResponseCodesAndVerbs[statusCode.toLowerCase()].orEmpty()
+            return allowedMethods.contains(method.name) || allowedMethods.contains("ALL")
+        }
+
+
+
+}
