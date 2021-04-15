d581bebc19a3e607570abb2534939d0bc094dfcb
Sophie Janssens <sophiej@backbase.com>
2021-04-15 21:35:15 +0200

    fix: should be thread safe

--- boat-maven-plugin/src/main/java/com/backbase/oss/boat/InputArtifact.java
+++ boat-maven-plugin/src/main/java/com/backbase/oss/boat/InputArtifact.java
@@ -2,10 +2,16 @@ package com.backbase.oss.boat;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 
 @Getter
 @Setter
+@Slf4j
 public class InputArtifact {
   private String groupId;
   private String artifactId;
@@ -13,6 +19,24 @@ public class InputArtifact {
   private String type;
   private String classifier;
   private String fileName;
-  private boolean isProcessed;
-  private boolean isUnzipped;
+  private boolean overWrite;
+  private boolean overWriteIfNewer;
+  private boolean needsProcessing;
+
+  public boolean isNeedsProcessing(File destFile, File originFile){
+    needsProcessing = overWrite || !destFile.exists() || (overWriteIfNewer && isNewer(destFile,originFile));
+    return needsProcessing;
+  }
+
+  private boolean isNewer( File destFile, File originFile )  {
+    try {
+      long destMod = Files.getLastModifiedTime( destFile.toPath() ).toMillis();
+      long originMod = Files.getLastModifiedTime( originFile.toPath() ).toMillis();
+      return  originMod > destMod;
+    } catch (IOException e) {
+      log.debug("Assuming artifact was not modified since artifact was last downloaded, cannot last read modified time");
+      return false;
+    }
+
+  }
 }
