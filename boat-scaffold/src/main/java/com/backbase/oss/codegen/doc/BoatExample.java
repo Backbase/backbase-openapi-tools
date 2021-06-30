7fb375547a118af5f32748f1af21f6db0ba5b33c
David Scourfield <daiscog@users.noreply.github.com>
2021-06-30 11:53:24 +0100

    #204 Guard against NPE in BoatExample.java

--- boat-scaffold/src/main/java/com/backbase/oss/codegen/doc/BoatExample.java
+++ boat-scaffold/src/main/java/com/backbase/oss/codegen/doc/BoatExample.java
@@ -38,6 +38,8 @@ public class BoatExample {
         if (example.getValue() instanceof JsonNode
                 || example.getValue() instanceof HashMap) {
             return Json.pretty(example.getValue());
+        } else if (example.getValue() == null) {
+            return "";
         } else {
             return example.getValue().toString();
         }
