fe57956f649c3feeea38c2ce7c90631d700ad0e5
Nicolas Torres <nicolast@backbase.com>
2021-07-05 16:02:47 +0200

    Removes unused templates

--- boat-scaffold/src/main/java/com/backbase/oss/codegen/marina/BoatMarinaGenerator.java
+++ boat-scaffold/src/main/java/com/backbase/oss/codegen/marina/BoatMarinaGenerator.java
@@ -34,35 +34,6 @@ public class BoatMarinaGenerator extends BoatStaticDocsGenerator   {
     public void processOpts() {
         super.processOpts();
          this.supportingFiles.add(new SupportingFile("api.js.handlebars", "api.js"));
-
-
-        // this.supportingFiles.add(new SupportingFile("css/jsontreeviewer.css", "css/jsontreeviewer.css"));
-        // this.supportingFiles.add(new SupportingFile("css/jsonschemaview.css", "css/jsonschemaview.css"));
-        // this.supportingFiles.add(new SupportingFile("css/prettify.css", "css/prettify.css"));
-        // this.supportingFiles.add(new SupportingFile("css/response.css", "css/response.css"));
-        // this.supportingFiles.add(new SupportingFile("css/styles.css", "css/styles.css"));
-
-        // this.supportingFiles.add(new SupportingFile("js/json_stringify_safe.js", "js/json_stringify_safe.js"));
-        // this.supportingFiles.add(new SupportingFile("js/js_bootstrap.js", "js/js_bootstrap.js"));
-        // this.supportingFiles.add(new SupportingFile("js/jsonformatter.js", "js/jsonformatter.js"));
-        // this.supportingFiles.add(new SupportingFile("js/jsonschema-ref-parser.js", "js/jsonschema-ref-parser.js"));
-        // this.supportingFiles.add(new SupportingFile("js/jsonschemaview.js", "js/jsonschemaview.js"));
-        // this.supportingFiles.add(new SupportingFile("js/jsontreeviewer.js", "js/jsontreeviewer.js"));
-        // this.supportingFiles.add(new SupportingFile("js/jsonschemamergeallof.js", "js/jsonschemamergeallof.js"));
-        // this.supportingFiles.add(new SupportingFile("js/marked.js", "js/marked.js"));
-        // this.supportingFiles.add(new SupportingFile("js/prettify.js", "js/prettify.js"));
-        // this.supportingFiles.add(new SupportingFile("js/webfontloader.js", "js/webfontloader.js"));
-
-        // this.supportingFiles.add(new SupportingFile("backbase-logo.svg", "backbase-logo.svg"));
-
-//        this.supportingFiles.add(new SupportingFile("index.js.handlebars", "spec/index.js"));
-        // this.supportingFiles.add(new SupportingFile("models.js.handlebars", "spec/models.js"));
-
-//         this.apiTemplateFiles.put("api.js.handlebars", ".js");
-        // this.modelTemplateFiles.put("model.js.handlebars", ".js");
-
-//        this.supportingFiles.add(new SupportingFile("api.js.handlebars", "api.js"));
-
     }
 
     @Override
