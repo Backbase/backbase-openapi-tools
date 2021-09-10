7584de13e9fcf43d0736485e2bb957076c77810b
dhananjay12 <dhananjay1204@gmail.com>
2021-09-10 07:05:01 +0200

    Upload specs to Boat bay server

---
+++ boat-maven-plugin/src/main/java/com/backbase/oss/boat/RadioMojo.java
@@ -0,0 +1,167 @@
+package com.backbase.oss.boat;
+
+import com.backbase.oss.boat.bay.client.ApiClient;
+import com.backbase.oss.boat.bay.client.api.BoatMavenPluginApi;
+import com.backbase.oss.boat.bay.client.model.BoatLintReport;
+import com.backbase.oss.boat.bay.client.model.UploadRequestBody;
+import com.backbase.oss.boat.bay.client.model.UploadSpec;
+import com.backbase.oss.boat.loader.OpenAPILoader;
+import com.backbase.oss.boat.loader.OpenAPILoaderException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationConfig;
+import feign.auth.BasicAuthRequestInterceptor;
+import io.swagger.v3.oas.models.OpenAPI;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.Data;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+@Mojo(name = "radio", requiresDependencyResolution = ResolutionScope.RUNTIME, threadSafe = true)
+@Slf4j
+public class RadioMojo extends AbstractMojo {
+
+    @Parameter(property = "groupId", defaultValue = "${project.groupId}", required = true)
+    String groupId;
+
+    @Parameter(property = "artifactId", defaultValue = "${project.artifactId}", required = true)
+    String artifactId;
+
+    @Parameter(property = "version", defaultValue = "${project.version}", required = true)
+    String version;
+
+    @Parameter(property = "basePath", required = true)
+    String basePath;
+
+    @Parameter(property = "portalKey", required = true)
+    String portalKey;
+
+    @Parameter(property = "sourceKey", required = true)
+    String sourceKey;
+
+    @Parameter(property = "username", required = false)
+    String username;
+
+    @Parameter(property = "password", required = false)
+    String password;
+
+    @Parameter(property = "specs")
+    SpecConfig[] specs;
+
+    @Parameter(name = "radioOutput", defaultValue = "${project.build.directory}/target/boat-radio-report")
+    File radioOutput;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        BasicAuthRequestInterceptor basicAuthRequestInterceptor = null;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        if(username != null && !username.isEmpty() && password != null && !password.isEmpty()){
+             basicAuthRequestInterceptor = new BasicAuthRequestInterceptor(username, password);
+        }else{
+            getLog().warn("No Authentication set");
+        }
+
+        List<UploadSpec> allSpecs = Arrays.stream(specs).map(this::mapToUploadSpec).collect(Collectors.toList());
+
+        ApiClient apiClient = new ApiClient().setBasePath(basePath);
+        if(basicAuthRequestInterceptor != null){
+            apiClient.addAuthorization("Basic Auth", basicAuthRequestInterceptor);
+        }
+
+        BoatMavenPluginApi api = apiClient.buildClient(BoatMavenPluginApi.class);
+
+        UploadRequestBody uploadRequestBody = new UploadRequestBody();
+        uploadRequestBody.setGroupId(groupId);
+        uploadRequestBody.setArtifactId(artifactId);
+        uploadRequestBody.setVersion(version);
+        uploadRequestBody.setSpecs(allSpecs);
+
+        List<BoatLintReport> reports = api.uploadSpec(portalKey,sourceKey,uploadRequestBody);
+
+        try {
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(getOutput(), "radioOutput.json"),reports);
+        } catch (IOException e) {
+            throw new MojoFailureException("Failed to write output", e);
+        }
+
+    }
+
+    private UploadSpec mapToUploadSpec(SpecConfig spec) {
+
+        OpenAPI openApi = null;
+        String contents = null;
+        File file = spec.getInputSpec();
+        try {
+            contents = IOUtils.toString(spec.getInputSpec().toURI(), Charset.defaultCharset());
+            openApi = OpenAPILoader.parse(contents);
+        } catch ( IOException e) {
+            String msg = "Invalid File Path: " + file.getName();
+            getLog().error(msg);
+            throw new RuntimeException(msg, e);
+        } catch (OpenAPILoaderException e) {
+            String msg = "Invalid Open Api file: " + file.getName();
+            getLog().error(msg);
+            throw new RuntimeException(msg, e);
+        }
+
+        String key = spec.getKey();
+        if (key == null || key.isEmpty()) {
+            key = file.getName().substring(0, file.getName().lastIndexOf("-"));
+        }
+
+        String name = spec.getName();
+        if (name == null || name.isEmpty()) {
+            name = file.getName();
+        }
+
+        UploadSpec uploadSpec = new UploadSpec();
+        uploadSpec.setFileName(file.getName());
+        uploadSpec.setKey(key);
+        uploadSpec.setName(name);
+        uploadSpec.setOpenApi(contents);
+
+
+        return uploadSpec;
+
+    }
+
+    @SneakyThrows
+    private File getOutput() {
+        if (radioOutput == null) {
+            radioOutput = new File("./target/boat-radio-report");
+        }
+        if(!radioOutput.exists()){
+            radioOutput.mkdir();
+        }
+        return radioOutput;
+    }
+
+
+}
+
+@Data
+class SpecConfig {
+
+    private String key;
+
+    private String name;
+
+    private File inputSpec;
+
+}
---
+++ boat-maven-plugin/src/test/java/com/backbase/oss/boat/RadioMojoTests.java
@@ -0,0 +1,200 @@
+package com.backbase.oss.boat;
+
+import com.backbase.oss.boat.bay.client.model.BoatLintReport;
+import com.backbase.oss.boat.bay.client.model.UploadRequestBody;
+import com.backbase.oss.boat.bay.client.model.UploadSpec;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+class RadioMojoTests {
+
+    static MockWebServer mockBackEnd;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeAll
+    @SneakyThrows
+    static void setUp() {
+        mockBackEnd = new MockWebServer();
+        mockBackEnd.start();
+    }
+
+    @Test
+    @SneakyThrows
+    void test() {
+
+        final String portalKey = "example";
+        final String sourceKey = "pet-store-bom";
+        final String specKey = "spec-key";
+        final String specValue = "spec-name";
+        final String groupId = "com.backbase.boat.samples";
+        final String artifactId = "pet-store-bom";
+        final String version = "2021.09";
+        final String fileName = "one-client-api-v1.yaml";
+
+        final BigDecimal reportId = BigDecimal.valueOf(10);
+        final String reportGrade = "A";
+
+        SpecConfig specConfig = new SpecConfig();
+        specConfig.setKey(specKey);
+        specConfig.setName(specValue);
+        specConfig.setInputSpec(getFile("/bundler/folder/" + fileName));
+
+        RadioMojo mojo = new RadioMojo();
+        mojo.groupId = groupId;
+        mojo.artifactId = artifactId;
+        mojo.version = version;
+        mojo.portalKey = portalKey;
+        mojo.sourceKey = sourceKey;
+        mojo.specs = new SpecConfig[]{specConfig};
+        mojo.basePath = String.format("http://localhost:%s", mockBackEnd.getPort());
+
+        final Dispatcher dispatcher = new Dispatcher() {
+            @SneakyThrows
+            @Override
+            public MockResponse dispatch(RecordedRequest request) {
+                switch (request.getPath()) {
+                    case "/api/boat/portals/" + portalKey + "/boat-maven-plugin/" + sourceKey + "/upload":
+
+                        UploadRequestBody requestBody = objectMapper.readValue(request.getBody().readUtf8(), UploadRequestBody.class);
+                        UploadSpec uploadSpec = requestBody.getSpecs().get(0);
+
+                        if (requestBody.getGroupId().equals(groupId) &&
+                                requestBody.getArtifactId().equals(artifactId) &&
+                                requestBody.getVersion().equals(version) &&
+                                uploadSpec.getKey().equals(specKey) &&
+                                uploadSpec.getName().equals(specValue) &&
+                                uploadSpec.getFileName().equals(fileName) &&
+                                uploadSpec.getOpenApi().length() > 0
+                        ) {
+                            log.info(uploadSpec.getOpenApi());
+                            BoatLintReport boatLintReport = new BoatLintReport();
+                            boatLintReport.setId(reportId);
+                            boatLintReport.setGrade(reportGrade);
+                            List<BoatLintReport> result = new ArrayList<>();
+                            result.add(boatLintReport);
+                            return new MockResponse().setResponseCode(200).setBody(objectMapper.writeValueAsString(result));
+                        } else {
+                            return new MockResponse().setResponseCode(400);
+                        }
+                }
+                return new MockResponse().setResponseCode(404);
+            }
+        };
+
+        mockBackEnd.setDispatcher(dispatcher);
+
+        mojo.execute();
+
+        File output = new File(mojo.radioOutput, "radioOutput.json");
+
+        assertTrue(output.exists());
+
+        List<BoatLintReport> result = Arrays.asList(objectMapper.readValue(output, BoatLintReport[].class));
+
+        assertTrue(result.size() == 1);
+        assertTrue(result.get(0).getId().equals(reportId));
+        assertTrue(result.get(0).getGrade().equals(reportGrade));
+
+    }
+
+
+    @Test
+    @SneakyThrows
+    void test_Empty_SpecKeyAndName() {
+
+        final String portalKey = "example";
+        final String sourceKey = "pet-store-bom";
+        final String groupId = "com.backbase.boat.samples";
+        final String artifactId = "pet-store-bom";
+        final String version = "2021.09";
+        final String fileName = "one-client-api-v2.yaml";
+
+        final BigDecimal reportId = BigDecimal.valueOf(20);
+        final String reportGrade = "B";
+
+        SpecConfig specConfig = new SpecConfig();
+        specConfig.setInputSpec(getFile("/bundler/folder/" + fileName));
+
+        RadioMojo mojo = new RadioMojo();
+        mojo.groupId = groupId;
+        mojo.artifactId = artifactId;
+        mojo.version = version;
+        mojo.portalKey = portalKey;
+        mojo.sourceKey = sourceKey;
+        mojo.specs = new SpecConfig[]{specConfig};
+        mojo.basePath = String.format("http://localhost:%s", mockBackEnd.getPort());
+
+        final Dispatcher dispatcher = new Dispatcher() {
+            @SneakyThrows
+            @Override
+            public MockResponse dispatch(RecordedRequest request) {
+                switch (request.getPath()) {
+                    case "/api/boat/portals/" + portalKey + "/boat-maven-plugin/" + sourceKey + "/upload":
+
+                        UploadRequestBody requestBody = objectMapper.readValue(request.getBody().readUtf8(), UploadRequestBody.class);
+                        UploadSpec uploadSpec = requestBody.getSpecs().get(0);
+
+                        String expectedDefaultKey  = fileName.substring(0, fileName.lastIndexOf("-"));
+                        String expectedDefaultName  = fileName;
+
+                        if (requestBody.getGroupId().equals(groupId) &&
+                                requestBody.getArtifactId().equals(artifactId) &&
+                                requestBody.getVersion().equals(version) &&
+                                uploadSpec.getKey().equals(expectedDefaultKey) &&
+                                uploadSpec.getName().equals(expectedDefaultName) &&
+                                uploadSpec.getFileName().equals(fileName) &&
+                                uploadSpec.getOpenApi().length() > 0
+                        ) {
+                            log.info(uploadSpec.getOpenApi());
+                            BoatLintReport boatLintReport = new BoatLintReport();
+                            boatLintReport.setId(reportId);
+                            boatLintReport.setGrade(reportGrade);
+                            List<BoatLintReport> result = new ArrayList<>();
+                            result.add(boatLintReport);
+                            return new MockResponse().setResponseCode(200).setBody(objectMapper.writeValueAsString(result));
+                        } else {
+                            return new MockResponse().setResponseCode(400);
+                        }
+                }
+                return new MockResponse().setResponseCode(404);
+            }
+        };
+
+        mockBackEnd.setDispatcher(dispatcher);
+
+        mojo.execute();
+
+        File output = new File(mojo.radioOutput, "radioOutput.json");
+
+        assertTrue(output.exists());
+
+        List<BoatLintReport> result = Arrays.asList(objectMapper.readValue(output, BoatLintReport[].class));
+
+        assertTrue(result.size() == 1);
+        assertTrue(result.get(0).getId().equals(reportId));
+        assertTrue(result.get(0).getGrade().equals(reportGrade));
+
+    }
+
+
+    private File getFile(String fileName) {
+        return new File(getClass().getResource(fileName).getFile());
+    }
+
+}
