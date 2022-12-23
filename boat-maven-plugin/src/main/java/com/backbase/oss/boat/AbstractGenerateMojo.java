69f5f57bff26ccade5b72ca8b68dd1c3d75da265
Bart Veenstra <bartv@backbase.com>
2022-12-23 11:10:24 +0100

    Added missing supporting files

--- boat-maven-plugin/src/main/java/com/backbase/oss/boat/AbstractGenerateMojo.java
+++ boat-maven-plugin/src/main/java/com/backbase/oss/boat/AbstractGenerateMojo.java
@@ -32,7 +32,7 @@ public abstract class AbstractGenerateMojo extends GenerateMojo {
         this.configOptions = options;
 
         if(isEmbedded) {
-            this.supportingFilesToGenerate = "ApiClient.java,BeanValidationException.java,RFC3339DateFormat.java,ServerConfiguration.java,ServerVariable.java,StringUtil.java,Authentication.java,HttpBasicAuth.java,HttpBearerAuth.java,ApiKeyAuth.java";
+            this.supportingFilesToGenerate = "ApiClient.java,BeanValidationException.java,RFC3339DateFormat.java,ServerConfiguration.java,ServerVariable.java,StringUtil.java,Authentication.java,HttpBasicAuth.java,HttpBearerAuth.java,ApiKeyAuth.java,JavaTimeFormatter.java";
         }
         super.execute();
     }
