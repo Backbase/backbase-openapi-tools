aecd97cf28f8522b7514e94813258e8809a63071
Aleksandr Endovitskii <aleksandre@backbase.com>
2023-05-26 10:25:45 +0200

    [BUS-603]: new angular mocks template [draft] [BUS-603]: updated tests for mocks [BUS-603]: changelog update

--- boat-scaffold/src/test/java/com/backbase/oss/codegen/angular/BoatAngularTemplatesTests.java
+++ boat-scaffold/src/test/java/com/backbase/oss/codegen/angular/BoatAngularTemplatesTests.java
@@ -121,7 +121,7 @@ class BoatAngularTemplatesTests {
     @Check
     public void withMocks() {
         assertThat(
-                findPattern(selectFiles("/api/.+\\.service\\.mocks\\.ts$"), "MocksProvider = /\\*#__PURE__\\*/ createMocks"),
+                findPattern(selectFiles("/api/.+\\.service\\.mocks\\.ts$"), "module.exports"),
                 equalTo(this.param.withMocks));
     }
 
