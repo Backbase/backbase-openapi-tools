316466ef5ef88304ce13cd26e732f82ac8084b3e
jasper linschoten <jasper@backbase.com>
2023-09-08 10:29:17 +0200

    Fix createApiComponent option in resttemplate is ignored

--- boat-scaffold/src/test/java/com/backbase/oss/codegen/java/BoatJavaCodeGenTests.java
+++ boat-scaffold/src/test/java/com/backbase/oss/codegen/java/BoatJavaCodeGenTests.java
@@ -1,26 +1,33 @@
 package com.backbase.oss.codegen.java;
 
-import static com.backbase.oss.codegen.java.BoatJavaCodeGen.*;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
+import io.swagger.parser.OpenAPIParser;
+import io.swagger.v3.parser.core.models.ParseOptions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openapitools.codegen.CliOption;
+import org.openapitools.codegen.ClientOptInput;
+import org.openapitools.codegen.DefaultGenerator;
+import org.openapitools.codegen.languages.SpringCodegen;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Map;
 
+import static com.backbase.oss.codegen.java.BoatJavaCodeGen.*;
 import static java.util.stream.Collectors.groupingBy;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasEntry;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.openapitools.codegen.CliOption;
-import org.openapitools.codegen.CodegenModel;
-import org.openapitools.codegen.CodegenParameter;
-import org.openapitools.codegen.CodegenProperty;
 
 class BoatJavaCodeGenTests {
 
+    static final String PROP_BASE = BoatJavaCodeGenTests.class.getSimpleName() + ".";
+    static final String TEST_OUTPUT = System.getProperty(PROP_BASE + "output", "target/boat-java-codegen-tests");
     @Test
     void clientOptsUnicity() {
         final BoatJavaCodeGen gen = new BoatJavaCodeGen();
@@ -43,6 +50,7 @@ class BoatJavaCodeGenTests {
         assertThat(gen.useJacksonConversion, is(false));
         assertThat(gen.useDefaultApiClient, is(true));
         assertThat(gen.restTemplateBeanName, is(nullValue()));
+        assertThat(gen.createApiComponent, is(true));
     }
 
     @Test
@@ -57,6 +65,7 @@ class BoatJavaCodeGenTests {
         options.put(USE_JACKSON_CONVERSION, "true");
         options.put(USE_DEFAULT_API_CLIENT, "false");
         options.put(REST_TEMPLATE_BEAN_NAME, "the-coolest-rest-template-in-this-universe");
+        options.put(CREATE_API_COMPONENT, "false");
 
         gen.processOpts();
 
@@ -66,6 +75,7 @@ class BoatJavaCodeGenTests {
         assertThat(gen.useJacksonConversion, is(true));
         assertThat(gen.useDefaultApiClient, is(false));
         assertThat(gen.restTemplateBeanName, is("the-coolest-rest-template-in-this-universe"));
+        assertThat(gen.createApiComponent, is(false));
     }
 
     @Test
@@ -91,19 +101,6 @@ class BoatJavaCodeGenTests {
     }
 
     @Test
-    void processOptsCreateApiComponent() {
-        final BoatJavaCodeGen gen = new BoatJavaCodeGen();
-        final Map<String, Object> options = gen.additionalProperties();
-
-        options.put(CREATE_API_COMPONENT, "false");
-
-        gen.setLibrary("resttemplate");
-        gen.processOpts();
-
-        assertThat(gen.createApiComponent, is(false));
-    }
-
-    @Test
     void processOptsUseProtectedFields() {
         final BoatJavaCodeGen gen = new BoatJavaCodeGen();
         final Map<String, Object> options = gen.additionalProperties();
@@ -115,4 +112,43 @@ class BoatJavaCodeGenTests {
         assertThat(gen.additionalProperties(), hasEntry("modelFieldsVisibility", "protected"));
     }
 
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void shouldHonourGenerateComponentAnnotation(boolean generate) throws InterruptedException, FileNotFoundException {
+
+        var input = new File("src/test/resources/boat-spring/openapi.yaml");
+        var output = TEST_OUTPUT + "/shouldNotGenerateComponentAnnotation/" + String.valueOf(generate);
+
+        final BoatJavaCodeGen gen = new BoatJavaCodeGen();
+        gen.setOutputDir(output);
+        gen.setInputSpec(input.getAbsolutePath());
+        gen.setApiPackage("com.backbase.test.api");
+        gen.setModelPackage("com.backbase.test.api.model");
+        gen.setInvokerPackage("com.backbase.test.api.invoker");
+        gen.setApiNameSuffix("ApiClient");
+
+        final Map<String, Object> options = gen.additionalProperties();
+        options.put("library", "resttemplate");
+        options.put(CREATE_API_COMPONENT, String.valueOf(generate));
+
+        var openApiInput = new OpenAPIParser()
+                .readLocation(input.getAbsolutePath(), null, new ParseOptions())
+                .getOpenAPI();
+        var clientOptInput = new ClientOptInput();
+        clientOptInput.config(gen);
+        clientOptInput.openAPI(openApiInput);
+
+        List<File> files = new DefaultGenerator().opts(clientOptInput).generate();
+
+        File apiFile = files.stream()
+                .filter(file -> file.getName().equals("PaymentsApiClient.java"))
+                .findFirst()
+                .get();
+        TypeDeclaration apiType = StaticJavaParser.parse(apiFile)
+                .findFirst(TypeDeclaration.class).get();
+
+        assertThat(gen.createApiComponent, is(generate));
+        assertThat(gen.getLibrary(), is("resttemplate"));
+        assertThat(apiType.getAnnotationByName("Component").isPresent(), is(generate));
+    }
 }
