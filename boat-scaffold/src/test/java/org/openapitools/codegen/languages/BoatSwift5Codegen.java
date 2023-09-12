4c4e068e065536d7493d77feac5ef5b7cee51c8a
Ebru Gungor <ebru@backbase.com>
2023-09-12 16:26:24 +0200

    fix: inheritance issue and creating sets rather than arrays

---
+++ boat-scaffold/src/test/java/org/openapitools/codegen/languages/BoatSwift5Codegen.java
@@ -0,0 +1,111 @@
+package org.openapitools.codegen.languages;
+
+import com.google.common.collect.Sets;
+import io.swagger.v3.oas.models.media.ArraySchema;
+import io.swagger.v3.oas.models.media.Schema;
+import org.jetbrains.annotations.NotNull;
+import org.openapitools.codegen.CodegenConfig;
+import org.openapitools.codegen.CodegenModel;
+import org.openapitools.codegen.CodegenProperty;
+import org.openapitools.codegen.meta.GeneratorMetadata;
+import org.openapitools.codegen.meta.Stability;
+import org.openapitools.codegen.model.ModelMap;
+import org.openapitools.codegen.model.ModelsMap;
+import org.openapitools.codegen.model.OperationsMap;
+import org.openapitools.codegen.utils.ModelUtils;
+
+import java.util.*;
+
+public class BoatSwift5Codegen extends Swift5ClientCodegen implements CodegenConfig {
+    private static final String LIBRARY_DBS = "dbsDataProvider";
+
+    /**
+     * Constructor for the BoatSwift5Codegen codegen module.
+     */
+    public BoatSwift5Codegen() {
+        super();
+        this.useOneOfInterfaces = true;
+
+        generatorMetadata = GeneratorMetadata.newBuilder(generatorMetadata)
+                .stability(Stability.STABLE)
+                .build();
+        supportedLibraries.put(LIBRARY_DBS, "HTTP client: DBSDataProvider");
+        setLibrary(LIBRARY_DBS);
+    }
+
+    @Override
+    public String getName() {
+        return "boat-swift5";
+    }
+
+    @Override
+    public String getHelp() {
+        return "Generates a BOAT Swift 5.x client library.";
+    }
+
+    @Override
+    public void processOpts() {
+        super.processOpts();
+        additionalProperties.put("useDBSDataProvider", getLibrary().equals(LIBRARY_DBS));
+    }
+
+    // Fix issues with generating arrays with Set.
+    @Override
+    public String getTypeDeclaration(Schema p) {
+        if (ModelUtils.isArraySchema(p)) {
+            ArraySchema ap = (ArraySchema) p;
+            Schema inner = ap.getItems();
+            return "[" + getTypeDeclaration(inner) + "]";
+        }
+        return super.getTypeDeclaration(p);
+    }
+
+    // Fix for inheritance bug
+    @Override
+    public Map<String, ModelsMap> postProcessAllModels(Map<String, ModelsMap> objs) {
+        Map<String, ModelsMap> postProcessedModels = super.postProcessAllModels(objs);
+
+        Iterator it = postProcessedModels.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, ModelsMap> entry = (Map.Entry)it.next();
+            CodegenModel model = ModelUtils.getModelByName((String)entry.getKey(), objs);
+            addParentProperties(model, objs);
+        }
+        return postProcessedModels;
+    }
+
+    // Fix for inheritance bug
+    private void addParentProperties(CodegenModel model, Map<String, ModelsMap> objs) {
+        Set<String> parents = model.allOf;
+        if (parents == null || parents.size() == 0) {
+            return;
+        }
+
+        for (String parent : parents) {
+            CodegenModel parentModel = ModelUtils.getModelByName(parent, objs);
+            fixInheritance(model, parentModel);
+
+            Set<String> parentsOfParent = parentModel.allOf;
+            if (parentsOfParent != null && parentsOfParent.size() > 0) {
+                // then recursively add all the parent properties of the parents.
+                addParentProperties(parentModel, objs);
+            }
+        }
+    }
+
+    /*
+     Fix for inheritance bug
+     There is no inheritance for Swift structs so we're adding all parent vars
+     recursively to the models allVars list while making sure we don't have duplicates.
+    */
+    private void fixInheritance(CodegenModel codegenModel, CodegenModel parentModel) {
+        if (parentModel != null) {
+            if (!parentModel.vars.isEmpty()) {
+                codegenModel.allVars.addAll(parentModel.vars);
+                codegenModel.requiredVars.addAll(parentModel.requiredVars);
+            }
+        }
+        codegenModel.removeAllDuplicatedProperty();
+    }
+}
+
