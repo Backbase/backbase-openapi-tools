b82184fde7706daace1bd2f5e93b71576ad148c8
Ebru Gungor <ebru@backbase.com>
2023-09-15 12:03:44 +0200

    chore: replace the wrongly added test class

---
+++ boat-scaffold/src/test/java/org/openapitools/codegen/languages/BoatSwift5CodegenTests.java
@@ -0,0 +1,34 @@
+package org.openapitools.codegen.languages;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+
+public class BoatSwift5CodegenTests {
+    BoatSwift5Codegen boatSwift5CodeGen = new BoatSwift5Codegen();
+
+    @Test
+    public void testGeneratorName() {
+        assertEquals(boatSwift5CodeGen.getName(), "boat-swift5");
+    }
+    @Test
+    public void testGetHelpMessage() {
+        assertEquals(boatSwift5CodeGen.getHelp(), "Generates a BOAT Swift 5.x client library.");
+    }
+
+    @Test
+    public void testProcessOptsSetDBSDataProvider() {
+        final BoatSwift5Codegen gen = new BoatSwift5Codegen();
+        final Map<String, Object> options = gen.additionalProperties();
+        gen.setLibrary("dbsDataProvider");
+
+        gen.processOpts();
+        gen.postProcess();
+
+        assertThat(gen.additionalProperties(), hasEntry("useDBSDataProvider", true));
+    }
+}
