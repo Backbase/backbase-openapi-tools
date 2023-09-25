09703ed3272a3e5e597c9dc278f6f92b03d1baca
George Nyakundi <georgen@backbase.com>
2023-09-25 17:47:25 +0200

    Cover new functionality with tests

--- boat-scaffold/src/test/java/org/openapitools/codegen/languages/BoatSwift5CodegenTests.java
+++ boat-scaffold/src/test/java/org/openapitools/codegen/languages/BoatSwift5CodegenTests.java
@@ -4,8 +4,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
 import java.util.Map;
 
+import io.swagger.v3.oas.models.media.ArraySchema;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.StringSchema;
+import org.checkerframework.checker.units.qual.A;
 import org.openapitools.codegen.languages.BoatSwift5Codegen;
 import org.junit.jupiter.api.Test;
 
@@ -25,12 +30,20 @@ public class BoatSwift5CodegenTests {
     @Test
     public void testProcessOptsSetDBSDataProvider() {
         final BoatSwift5Codegen gen = new BoatSwift5Codegen();
-        final Map<String, Object> options = gen.additionalProperties();
         gen.setLibrary("dbsDataProvider");
 
         gen.processOpts();
         gen.postProcess();
 
         assertThat(gen.additionalProperties(), hasEntry("useDBSDataProvider", true));
+    }
+    @Test
+    public void testGetTypeDeclaration() {
+
+        final ArraySchema childSchema = new ArraySchema().items(new StringSchema());
+
+        final BoatSwift5Codegen codegen = new BoatSwift5Codegen();
+
+        assertEquals(codegen.getTypeDeclaration(childSchema),"[String]");
     }
 }
