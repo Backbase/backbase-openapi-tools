dde9a4c4e3807b71e9eb843ec05763a672d6d466
George Nyakundi <georgen@backbase.com>
2023-10-03 15:01:21 +0200

    Uncomment failing test

--- boat-scaffold/src/test/java/com/backbase/oss/codegen/java/BoatSpringCodeGenTests.java
+++ boat-scaffold/src/test/java/com/backbase/oss/codegen/java/BoatSpringCodeGenTests.java
@@ -267,7 +267,7 @@ class BoatSpringCodeGenTests {
         var compiler = new MavenProjectCompiler(BoatSpringCodeGenTests.class.getClassLoader());
         var projectDir = new File(output);
         int compilationStatus = compiler.compile(projectDir);
-//        assertEquals(0, compilationStatus);
+        assertEquals(0, compilationStatus);
 
         // verify
         ClassLoader projectClassLoader = compiler.getProjectClassLoader(projectDir);
