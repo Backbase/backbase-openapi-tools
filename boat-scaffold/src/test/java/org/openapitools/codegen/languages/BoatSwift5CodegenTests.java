05595eb239437c92d9005084dd1a1ab6177e6247
George Nyakundi <georgen@backbase.com>
2023-10-24 10:10:18 +0200

    More test assertions

--- boat-scaffold/src/test/java/org/openapitools/codegen/languages/BoatSwift5CodegenTests.java
+++ boat-scaffold/src/test/java/org/openapitools/codegen/languages/BoatSwift5CodegenTests.java
@@ -1,19 +1,26 @@
 package org.openapitools.codegen.languages;
 
+import io.swagger.parser.OpenAPIParser;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Paths;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.media.*;
 
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.parser.core.models.ParseOptions;
+
 import io.swagger.v3.parser.util.SchemaTypeUtil;
 import org.junit.jupiter.api.Test;
 import org.openapitools.codegen.CodegenModel;
 import org.openapitools.codegen.CodegenProperty;
+import org.openapitools.codegen.InlineModelResolver;
 import org.openapitools.codegen.model.ModelMap;
 import org.openapitools.codegen.model.ModelsMap;
+import org.openapitools.codegen.utils.ModelUtils;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -169,25 +176,12 @@ public class BoatSwift5CodegenTests {
 
     @Test
     public void testFromModel() {
-        final ByteArraySchema byteArraySchema = new ByteArraySchema();
-//        byteArraySchema.se
-//        byteArray.addf
-        CodegenProperty codegenProperty = new CodegenProperty();
-        codegenProperty.isMap = true;
-        codegenProperty.isFreeFormObject = true;
 
-        final CodegenModel parent = new CodegenModel();
-        parent.setName("parent");
-        parent.classname = "parent";
-        parent.parentSchema = "sample";
+        final ObjectSchema objectSchema = new ObjectSchema();
+        objectSchema.setDescription("Sample ObjectSchema");
+//        objectSchema.
 
-
-        List<CodegenProperty> codegenProperties = new ArrayList<>();
-        codegenProperties.add(codegenProperty);
-
-        parent.setAllVars(codegenProperties);
-
-
+        final ArraySchema nestedArraySchema = new ArraySchema().items(new ObjectSchema());
 
         final Schema schema = new Schema()
                 .description("a sample model")
@@ -195,20 +189,90 @@ public class BoatSwift5CodegenTests {
                 .addProperty("name", new StringSchema())
                 .addProperty("createdAt", new DateTimeSchema())
                 .addProperty("binary", new BinarySchema())
-                .addProperty("byte", byteArraySchema)
+                .addProperty("byte", new ByteArraySchema())
                 .addProperty("uuid", new UUIDSchema())
                 .addProperty("dateOfBirth", new DateSchema())
+                .addProperty("content", objectSchema)
+                .addProperty("nested", nestedArraySchema)
                 .addRequiredItem("id")
                 .addRequiredItem("name")
                 .name("sample")
                 .discriminator(new Discriminator().propertyName("test"));
 
-        parent.setTypeProperties(schema);
-
+        OpenAPI openAPI = createOpenAPIWithOneSchema("sample", schema);
+        boatSwift5CodeGen.setOpenAPI(openAPI);
         final CodegenModel cm = boatSwift5CodeGen.fromModel("sample", schema);
-        assertEquals(schema.getName(), cm.name);
-        assertTrue(cm.hasRequired);
 
+        assertEquals(cm.name, "sample");
+        assertEquals(cm.classname, "Sample");
+        assertEquals(cm.description, "a sample model");
+        assertEquals(cm.vars.size(), 9);
+        assertEquals(cm.getDiscriminatorName(),"test");
+
+        final CodegenProperty property1 = cm.vars.get(0);
+        assertEquals(property1.baseName, "id");
+        assertEquals(property1.dataType, "Int64");
+        assertEquals(property1.name, "id");
+        assertNull(property1.defaultValue);
+        assertEquals(property1.baseType, "Int64");
+        assertTrue(property1.required);
+        assertTrue(property1.isPrimitiveType);
+        assertFalse(property1.isContainer);
+
+        final CodegenProperty property2 = cm.vars.get(1);
+        assertEquals(property2.baseName, "name");
+        assertEquals(property2.dataType, "String");
+        assertEquals(property2.name, "name");
+        assertNull(property2.defaultValue);
+        assertEquals(property2.baseType, "String");
+        assertTrue(property2.required);
+        assertTrue(property2.isPrimitiveType);
+        assertFalse(property2.isContainer);
+
+        final CodegenProperty property3 = cm.vars.get(2);
+        assertEquals(property3.baseName, "createdAt");
+        assertEquals(property3.dataType, "Date");
+        assertEquals(property3.name, "createdAt");
+        assertNull(property3.defaultValue);
+        assertEquals(property3.baseType, "Date");
+        assertFalse(property3.required);
+        assertFalse(property3.isContainer);
+
+        final CodegenProperty property4 = cm.vars.get(3);
+        assertEquals(property4.baseName, "binary");
+        assertEquals(property4.dataType, "URL");
+        assertEquals(property4.name, "binary");
+        assertNull(property4.defaultValue);
+        assertEquals(property4.baseType, "URL");
+        assertFalse(property4.required);
+        assertFalse(property4.isContainer);
+
+        final CodegenProperty property5 = cm.vars.get(4);
+        assertEquals(property5.baseName, "byte");
+        assertEquals(property5.dataType, "Data");
+        assertEquals(property5.name, "byte");
+        assertNull(property5.defaultValue);
+        assertEquals(property5.baseType, "Data");
+        assertFalse(property5.required);
+        assertFalse(property5.isContainer);
+
+        final CodegenProperty property6 = cm.vars.get(5);
+        assertEquals(property6.baseName, "uuid");
+        assertEquals(property6.dataType, "UUID");
+        assertEquals(property6.name, "uuid");
+        assertNull(property6.defaultValue);
+        assertEquals(property6.baseType, "UUID");
+        assertFalse(property6.required);
+        assertFalse(property6.isContainer);
+
+        final CodegenProperty property7 = cm.vars.get(6);
+        assertEquals(property7.baseName, "dateOfBirth");
+        assertEquals(property7.dataType, "Date");
+        assertEquals(property7.name, "dateOfBirth");
+        assertNull(property7.defaultValue);
+        assertEquals(property7.baseType, "Date");
+        assertFalse(property7.required);
+        assertFalse(property7.isContainer);
 
     }
 
@@ -274,4 +338,33 @@ public class BoatSwift5CodegenTests {
         openAPI.setServers(Collections.singletonList(server));
         return openAPI;
     }
+
+    static OpenAPI parseFlattenSpec(String specFilePath) {
+        OpenAPI openAPI = parseSpec(specFilePath);
+        return flatten(openAPI);
+    }
+
+    static OpenAPI parseSpec(String specFilePath) {
+        OpenAPI openAPI = new OpenAPIParser().readLocation(specFilePath, null, new ParseOptions()).getOpenAPI();
+        // Invoke helper function to get the original swagger version.
+        // See https://github.com/swagger-api/swagger-parser/pull/1374
+        // Also see https://github.com/swagger-api/swagger-parser/issues/1369.
+        ModelUtils.getOpenApiVersion(openAPI, specFilePath, null);
+        return openAPI;
+    }
+    static OpenAPI flatten(OpenAPI openAPI) {
+
+        if (openAPI.getComponents() == null) {
+            openAPI.setComponents(new Components());
+        }
+
+        if (openAPI.getComponents().getSchemas() == null) {
+            openAPI.getComponents().setSchemas(new HashMap<String, Schema>());
+        }
+        return  openAPI;
+
+//        flattenPaths();
+//        flattenComponents();
+    }
+
 }
