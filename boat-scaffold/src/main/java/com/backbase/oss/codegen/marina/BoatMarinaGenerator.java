8d4d2578d546ff41fd1a583c7c38568f0145dc24
Fredy Sierra <fredy@backbase.com>
2024-10-11 12:26:37 +0200

    GENERATE_ALIAS_AS_MODEL as true to generate information about array types.

--- boat-scaffold/src/main/java/com/backbase/oss/codegen/marina/BoatMarinaGenerator.java
+++ boat-scaffold/src/main/java/com/backbase/oss/codegen/marina/BoatMarinaGenerator.java
@@ -21,7 +21,7 @@ public class BoatMarinaGenerator extends BoatStaticDocsGenerator {
 
         embeddedTemplateDir = templateDir = NAME;
         cliOptions.add(new CliOption(CodegenConstants.GENERATE_ALIAS_AS_MODEL, CodegenConstants.GENERATE_ALIAS_AS_MODEL));
-        additionalProperties.put(CodegenConstants.GENERATE_ALIAS_AS_MODEL, false);
+        additionalProperties.put(CodegenConstants.GENERATE_ALIAS_AS_MODEL, true);
         additionalProperties.put("appName", "BOAT Marina Documentation");
         additionalProperties.put("appDescription", "For a collection of doc(k)s");
         additionalProperties.put("infoUrl", "https://backbase.github.io/backbase-openapi-tools/");
@@ -31,6 +31,7 @@ public class BoatMarinaGenerator extends BoatStaticDocsGenerator {
         typeAliases = new HashMap<>();
         HandlebarsEngineAdapter templatingEngine = new BoatHandlebarsEngineAdapter();
         setTemplatingEngine(templatingEngine);
+
     }
 
 
