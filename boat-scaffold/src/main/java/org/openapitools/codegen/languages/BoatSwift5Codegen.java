19cdb0c39c0e394a5d833e531a4e50c41721f46a
Mirko Tomic <mirko@m-quart.com>
2025-06-16 16:32:18 +0200

    Create self contained patch with URLEncoding extension

--- boat-scaffold/src/main/java/org/openapitools/codegen/languages/BoatSwift5Codegen.java
+++ boat-scaffold/src/main/java/org/openapitools/codegen/languages/BoatSwift5Codegen.java
@@ -68,6 +68,7 @@ public class BoatSwift5Codegen extends Swift5ClientCodegen implements CodegenCon
         super.processOpts();
         additionalProperties.put("useDBSDataProvider", getLibrary().equals(LIBRARY_DBS));
         supportingFiles.add(new SupportingFile("AnyCodable.swift.mustache", sourceFolder, "AnyCodable.swift"));
+        supportingFiles.add(new SupportingFile("URLEncoding+ExplodedParameters.swift.mustache", sourceFolder, "URLEncoding+ExplodedParameters.swift"));
 
         if (additionalProperties.containsKey(DEPENDENCY_MANAGEMENT)) {
             Object dependenciesAsObject = additionalProperties.get(DEPENDENCY_MANAGEMENT);
